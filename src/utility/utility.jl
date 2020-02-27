a7c34ea9a267042255b0cf6d85e26d9b02095722
Jutho Haegeman <jutho.haegeman@ugent.be>
2020-02-27 22:11:49 +0100

    add useful utilities to work with generic mps tensors

--- src/utility/utility.jl
+++ src/utility/utility.jl
@@ -1,3 +1,32 @@
+function _permute_front(t::AbstractTensorMap) # make TensorMap{S,N₁+N₂-1,1}
+    I = TensorKit.allind(t) # = (1:N₁+N₂...,)
+    if BraidingStyle(sectortype(t)) isa SymmetricBraiding
+        permute(t, Base.front(I), (I[end],))
+    else
+        levels = I
+        braid(t, levels, Base.front(I), (I[end],))
+    end
+end
+function _permute_tail(t::AbstractTensorMap) # make TensorMap{S,1,N₁+N₂-1}
+    I = TensorKit.allind(t) # = (1:N₁+N₂...,)
+    if BraidingStyle(sectortype(t)) isa SymmetricBraiding
+        permute(t, (I[1],), Base.tail(I))
+    else
+        levels = I
+        braid(t, levels, (I[1],), Base.tail(I))
+    end
+end
+function _permute_as(t1::AbstractTensorMap, t2::AbstractTensorMap)
+    if BraidingStyle(sectortype(t1)) isa SymmetricBraiding
+        permute(t1, TensorKit.codomainind(t2), TensorKit.domainind(t2))
+    else
+        levels = allind(t1)
+        braid(t1, TensorKit.codomainind(t2), TensorKit.domainind(t2))
+    end
+end
+_firstspace(t::AbstractTensorMap) = space(t, 1)
+_lastspace(t::AbstractTensorMap) = space(t, numind(t))
+
 "
 Decomposes fun into a sum of exponentials
 (coeff,exponents)=exp_decomp(fun;fitdist,numexp);
