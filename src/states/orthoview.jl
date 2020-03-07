b1f0b9a05b69bc32773c53d606505a582ab185fe
maarten <maarten.vandamme@ugent.be>
2020-03-07 15:13:14 +0100

    new orthoview interface (can't be used by finitempo)

--- src/states/orthoview.jl
+++ src/states/orthoview.jl
@@ -1,3 +1,9 @@
+struct AView{S}
+    parent::S
+end
+
+Base.getindex(v::AView,i::Int) = v.parent.tensors[i]
+
 struct ACView{S}
     parent::S
 end
@@ -6,14 +12,31 @@ function Base.getindex(v::ACView,i::Int)
     leftorth!(v.parent,i,normalize=false)
     rightorth!(v.parent,i,normalize=false)
 
-    return v.parent[i]
+    return v.parent.tensors[i]
 end
 
-function Base.setindex!(v::ACView,vec,i::Int)
+function Base.setindex!(v::ACView,vec::GenericMPSTensor,i::Int)
     leftorth!(v.parent,i,normalize=false)
     rightorth!(v.parent,i,normalize=false)
 
-    v.parent[i] = vec
+    v.parent.tensors[i] = vec;
+    v.parent.centerpos = i:i;
+end
+
+function Base.setindex!(v::ACView,vec::Tuple{<:GenericMPSTensor,<:GenericMPSTensor},i::Int)
+    leftorth!(v.parent,i,normalize=false)
+    rightorth!(v.parent,i,normalize=false)
+
+    (a,b) = vec
+    if isa(a,MPSBondTensor) #c/ar
+        v.parent.tensors[i] = _permute_front(a*_permute_tail(b))
+    else #al/c
+        @assert isa(b,MPSBondTensor)
+
+        v.parent.tensors[i] = a*b;
+    end
+
+    v.parent.centerpos = i:i;
 end
 
 struct ALView{S}
@@ -21,17 +44,26 @@ struct ALView{S}
 end
 
 function Base.getindex(v::ALView,i::Int)
-    i == length(v.parent) && throw(ArgumentError("out of bounds"))
-    leftorth!(v.parent,i+1,normalize = false);
-
-    v.parent[i]
+    if i == length(v.parent)
+        leftorth!(v.parent,i,normalize = false);
+        (AL,C) = leftorth(v.parent.tensors[i])
+        return AL;
+    else
+        leftorth!(v.parent,i+1,normalize = false);
+        return v.parent.tensors[i]
+    end
 end
 
 function Base.setindex!(v::ALView,vec,i::Int)
-    i == length(v.parent) && throw(ArgumentError("out of bounds"))
-    leftorth!(v.parent,i+1,normalize = false);
+    if i == length(v.parent)
+        leftorth!(v.parent,i,normalize = false);
+        (AL,C) = leftorth(v.parent.tensors[i])
+        v.parent.tensors[i] = vec*C;
+    else
+        leftorth!(v.parent,i+1,normalize = false);
 
-    v.parent[i] = vec
+        v.parent.tensors[i] = vec
+    end
 end
 
 struct ARView{S}
@@ -39,17 +71,26 @@ struct ARView{S}
 end
 
 function Base.getindex(v::ARView,i::Int)
-    i == 1 && throw(ArgumentError("out of bounds"))
-    rightorth!(v.parent,i-1,normalize = false);
-
-    v.parent[i]
+    if i == 1
+        rightorth!(v.parent,i,normalize=false);
+        (C,AR) = rightorth(_permute_tail(v.parent.tensors[1]));
+        return _permute_front(AR);
+    else
+        rightorth!(v.parent,i-1,normalize = false);
+        return v.parent.tensors[i]
+    end
 end
 
 function Base.setindex!(v::ARView,vec,i::Int)
-    i == 1 && throw(ArgumentError("out of bounds"))
-    rightorth!(v.parent,i-1,normalize = false);
+    if i == 1
+        rightorth!(v.parent,i,normalize=false);
+        (C,AR) = rightorth(_permute_tail(v.parent.tensors[1]));
 
-    v.parent[i] = vec
+        v.parent.tensors[i] = _permute_front(C*_permute_tail(vec))
+    else
+        rightorth!(v.parent,i-1,normalize = false);
+        v.parent.tensors[i] = vec
+    end
 end
 
 
@@ -60,14 +101,18 @@ end
 function Base.getindex(v::CRView,i::Int)
     leftorth!(v.parent,i,normalize=false)
     rightorth!(v.parent,i,normalize=false)
-    (AL,C) = leftorth(v.parent[i]);
+
+    (AL,C) = leftorth(v.parent.tensors[i]);
+
     C
 end
 
 function Base.setindex!(v::CRView,vec,i::Int)
     leftorth!(v.parent,i,normalize=false)
     rightorth!(v.parent,i,normalize=false)
-    (AL,C) = leftorth(v.parent[i]);
 
-    v.parent[i] = AL*vec
+    (AL,C) = leftorth(v.parent.tensors[i]);
+
+    v.parent.tensors[i] = AL*vec
+    v.parent.centerpos = i:i
 end
