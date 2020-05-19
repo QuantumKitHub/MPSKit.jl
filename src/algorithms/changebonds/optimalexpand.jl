fc605d5a3f38887772f3422a4101548307264d4a
maarten <maarten.vandamme@ugent.be>
2020-05-19 17:06:47 +0200

    needed it for peps

--- src/algorithms/changebonds/optimalexpand.jl
+++ src/algorithms/changebonds/optimalexpand.jl
@@ -28,7 +28,7 @@ function changebonds(state::InfiniteMPS, H::Hamiltonian,alg::OptimalExpand,pars=
     #do the actual expansion
     for i in 1:length(state)
         al = MPSKit._permute_tail(TensorKit.catdomain(state.AL[i],pexp[i][1]))
-        lz = TensorMap(zeros,space(pexp[i-1][1],3)',domain(al))
+        lz = TensorMap(zeros,_lastspace(pexp[i-1][1])',domain(al))
         state.AL[i] = MPSKit._permute_front(TensorKit.catcodomain(al,lz))
 
         ar = MPSKit._permute_front(TensorKit.catcodomain(MPSKit._permute_tail(state.AR[i+1]),pexp[i][2]))
@@ -37,7 +37,7 @@ function changebonds(state::InfiniteMPS, H::Hamiltonian,alg::OptimalExpand,pars=
 
         l = TensorMap(zeros,codomain(state.CR[i]),space(pexp[i][2],1))
         state.CR[i] = TensorKit.catdomain(state.CR[i],l)
-        r = TensorMap(zeros,space(pexp[i][1],3)',domain(state.CR[i]))
+        r = TensorMap(zeros,_lastspace(pexp[i][1])',domain(state.CR[i]))
         state.CR[i] = TensorKit.catcodomain(state.CR[i],r)
 
         state.AC[i] = state.AL[i]*state.CR[i]
@@ -48,6 +48,52 @@ function changebonds(state::InfiniteMPS, H::Hamiltonian,alg::OptimalExpand,pars=
     return state,pars
 end
 
+
+function MPSKit.changebonds(state::MPSMultiline, H,alg::OptimalExpand,pars=params(state,H))
+    #=
+        todo : merge this with the MPSCentergauged implementation
+    =#
+    #determine optimal expansion spaces around bond i
+    exps = map(Iterators.product(1:size(state,1),1:size(state,2))) do (i,j)
+        ACAR = _permute_front(state.AC[i,j])*_permute_tail(state.AR[i,j+1])
+        AC2 = ac2_prime(ACAR,i,j,state,pars)
+
+        #Calculate nullspaces for AL and AR
+        NL = leftnull(state.AL[i,j])
+        NR = rightnull(_permute_tail(state.AR[i,j+1]))
+
+        #Use this nullspaces and SVD decomposition to determine the optimal expansion space
+        intermediate = adjoint(NL)*AC2*adjoint(NR)
+        (U,S,V) = tsvd(intermediate,trunc=alg.trscheme,alg=TensorKit.SVD())
+
+        (NL*U,V*NR)
+    end
+
+    pexp = PeriodicArray(collect(exps));
+
+    #do the actual expansion
+    for (i,j) in Iterators.product(1:size(state,1),1:size(state,2))
+        al = _permute_tail(TensorKit.catdomain(state.AL[i,j],pexp[i,j][1]))
+        lz = TensorMap(zeros,_lastspace(pexp[i,j-1][1])',domain(al))
+
+        state.AL[i,j] = _permute_front(TensorKit.catcodomain(al,lz))
+
+        ar = _permute_front(TensorKit.catcodomain(_permute_tail(state.AR[i,j+1]),pexp[i,j][2]))
+        rz = TensorMap(zeros,codomain(ar),space(pexp[i,j+1][2],1))
+        state.AR[i,j+1] = TensorKit.catdomain(ar,rz)
+
+        l = TensorMap(zeros,codomain(state.CR[i,j]),space(pexp[i,j][2],1))
+        state.CR[i,j] = TensorKit.catdomain(state.CR[i,j],l)
+        r = TensorMap(zeros,_lastspace(pexp[i,j][1])',domain(state.CR[i,j]))
+        state.CR[i,j] = TensorKit.catcodomain(state.CR[i,j],r)
+
+        state.AC[i,j] = state.AL[i,j]*state.CR[i,j]
+    end
+
+    poison!(pars)
+
+    return state,pars
+end
 function changebonds(state::Union{FiniteMPS,MPSComoving}, H::Hamiltonian,alg::OptimalExpand,pars=params(state,H))
     #inspired by the infinite mps algorithm, alternative is to use https://arxiv.org/pdf/1501.05504.pdf
     #didn't use the paper because generically it'd be annoying to implement (again having to fuse and stuff)
