31adfba96e966e9a15ab2a8f0ec4d3003c58880c
Gertian <Gertian.Roose@Ugent.be>
2020-07-07 12:46:37 +0200

    Added a function that returns sigma and pi order parameters for CGN model

--- src/models/grossneveu.jl
+++ src/models/grossneveu.jl
@@ -46,7 +46,6 @@ function su2u1_grossneveu(;g2SPT=0,g2AFM=0)
     blocks(O_op)[SU₂(0)×U₁(-1)]    =  -1*ones(1,1)
     blocks(O_op)[SU₂(0)×U₁(1)]    =  1*ones(1,1)
 
-
     #=
     BlockHamiltonian([ SimpleLocalMPO([LK , Cplus, RK]) ,
             SimpleLocalMPO([-0.25*g2SPT^2*Ldiffsq, Cdiffsq, Rdiffsq]) ,
@@ -58,5 +57,28 @@ function su2u1_grossneveu(;g2SPT=0,g2AFM=0)
     MPOHamiltonian([-0.25*g2SPT^2*Ldiffsq, Cdiffsq, Rdiffsq]) +
     MPOHamiltonian([-0.5*g2AFM^2*O_op*O_op]) +
     MPOHamiltonian([+0.5*g2AFM^2*O_op, O_op])
+end
 
+function su2u1_orderpars()
+    ph       = ℂ[SU₂×U₁]( (1//2,0)=>1, (0,-1)=>1, (0,1)=>1 )
+    onleg    = ℂ[SU₂×U₁]( (1//2,-1)=>1, (1//2,1)=>1 )
+    unit     = oneunit(ph)
+
+    LK = TensorMap(ones, ComplexF64, unit*ph, onleg*ph)
+    blocks(LK)[SU₂(0)×U₁(-1)]   =  ones(1,1)*im*2/sqrt(2)
+    blocks(LK)[SU₂(1//2)×U₁(0)] =  [im -im]
+    blocks(LK)[SU₂(0)×U₁(1)]    =  ones(1,1)*im*2/sqrt(2)
+
+    RK = TensorMap(ones, ComplexF64, onleg*ph, unit*ph)
+    blocks(RK)[SU₂(0)×U₁(-1)][:]    =  ones(1,1)*2/sqrt(2)
+    blocks(RK)[SU₂(1//2)×U₁(0)][:] =  [-1, 1][:]
+    blocks(RK)[SU₂(0)×U₁(1)][:]    =  ones(1,1)*2/sqrt(2)
+
+    #and now with the extra O(4) breaking part ie the O operator
+    O_op = TensorMap(zeros, ComplexF64, unit*ph, unit*ph)
+    blocks(O_op)[SU₂(1//2)×U₁(0)] = -zeros(1,1)
+    blocks(O_op)[SU₂(0)×U₁(-1)]    =  -1*ones(1,1)
+    blocks(O_op)[SU₂(0)×U₁(1)]    =  1*ones(1,1)
+
+    return repeat(MPOHamiltonian([LK, RK]),2) , repeat(MPOHamiltonian([O_op]),2)
 end
