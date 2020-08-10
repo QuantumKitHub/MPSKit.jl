c859e94eff9d1a98e4c2a3950a8ee4f09d928830
maarten <maarten.vandamme@ugent.be>
2020-08-10 16:34:52 +0200

    less excitation code

--- src/algorithms/excitation/quasiparticleexcitation.jl
+++ src/algorithms/excitation/quasiparticleexcitation.jl
@@ -4,8 +4,7 @@
 =#
 
 include("excitransfers.jl")
-include("heff_infinite.jl")
-include("heff_finite.jl")
+
 "
     quasiparticle_excitation calculates the energy of the first excited state at momentum 'moment'
 "
@@ -36,7 +35,7 @@ function quasiparticle_excitation(hamiltonian::Hamiltonian, mpsleft::FiniteMPS, 
     return Es,Vs
 end
 
-
+#give it a vector of momentum points
 function quasiparticle_excitation(hamiltonian::Hamiltonian, momenta::AbstractVector, mpsleft::InfiniteMPS, paramsleft=params(mpsleft,hamiltonian), mpsright::InfiniteMPS=mpsleft, paramsright=paramsleft;
     num=1,verbose=Defaults.verbose,kwargs...)
 
@@ -54,4 +53,50 @@ function quasiparticle_excitation(hamiltonian::Hamiltonian, momenta::AbstractVec
     Bp = permutedims(reduce(hcat,map(x->x[2][1:num],fetched)));
 
     return Ep,Bp
+end
+
+function effective_excitation_hamiltonian(ham::MPOHamiltonian, exci::QP,pars=params(exci,ham))
+    odim = ham.odim;
+
+    Bs = [exci[i] for i in 1:length(exci)];
+    toret = zero.(Bs);
+
+    #do necessary contractions
+    for i = 1:length(exci)
+        for (j,k) in keys(ham,i)
+            @tensor toret[i][-1,-2,-3,-4] +=    leftenv(pars.lpars,i,exci.left_gs)[j][-1,1,2]*
+                                                Bs[i][2,3,-3,4]*
+                                                ham[i,j,k][1,-2,5,3]*
+                                                rightenv(pars.rpars,i,exci.right_gs)[k][4,5,-4]
+
+            # <B|H|B>-<H>
+            en = @tensor    conj(exci.left_gs.AC[i][11,12,13])*
+                            leftenv(pars.lpars,i,exci.left_gs)[j][11,1,2]*
+                            exci.left_gs.AC[i][2,3,4]*
+                            ham[i,j,k][1,12,5,3]*
+                            rightenv(pars.lpars,i,exci.left_gs)[k][4,5,13]
+
+            toret[i] -= Bs[i]*en
+            if i>1 || exci isa InfiniteQP
+                @tensor toret[i][-1,-2,-3,-4] +=    pars.lBs[mod1(i-1,end)][j][-1,1,-3,2]*
+                                                    exci.right_gs.AR[i][2,3,4]*
+                                                    ham[i,j,k][1,-2,5,3]*
+                                                    rightenv(pars.rpars,i,exci.right_gs)[k][4,5,-4]
+            end
+            if i<length(exci.left_gs) || exci isa InfiniteQP
+                @tensor toret[i][-1,-2,-3,-4] +=    leftenv(pars.lpars,i,exci.left_gs)[j][-1,1,2]*
+                                                    exci.left_gs.AL[i][2,3,4]*
+                                                    ham[i,j,k][1,-2,5,3]*
+                                                    pars.rBs[mod1(i+1,end)][k][4,-3,5,-4]
+            end
+        end
+
+    end
+
+    toret_vec = similar(exci);
+    for i in 1:length(exci)
+        toret_vec[i] = toret[i]
+    end
+    return toret_vec
+
 end
--- src/environments/qpenv.jl
+++ src/environments/qpenv.jl
@@ -3,19 +3,9 @@ nothing fancy - only used internally (and therefore cryptic) - stores some parti
 seperates out this bit of logic from effective_excitation_hamiltonian (now more readable)
 can also - potentially - partially reuse this in other algorithms
 =#
-struct InfiniteQPEnv{A,B} <: Cache
-    lBsc::Array{A,1}
-    rBsc::Array{A,1}
-    lBsE::A
-    rBsE::A
-
-    lpars::B
-    rpars::B
-end
-
-struct FiniteQPEnv{A,B} <: Cache
-    lBsc::Array{A,1}
-    rBsc::Array{A,1}
+struct QPEnv{A,B} <: Cache
+    lBs::Array{A,1}
+    rBs::Array{A,1}
 
     lpars::B
     rpars::B
@@ -25,71 +15,94 @@ function params(exci::InfiniteQP,ham::MPOHamiltonian,lpars=params(exci.left_gs,h
     ids = collect(Iterators.filter(x->isid(ham,x),2:ham.odim-1));
 
     #build lBs(c)
-    lBs = [ TensorMap(zeros,eltype(exci.left_gs),
-                    space(lpars.lw[1,k],1)*space(lpars.lw[1,k],2),
-                    space(exci[1],3)'*space(rpars.lw[1,k],3)') for k in 1:ham.odim]
-    lBsc = typeof(lBs)[]
+    lB_cur = [ TensorMap(zeros,eltype(exci),
+                    virtualspace(exci.left_gs,0)*ham.domspaces[1,k]',
+                    space(exci[1],3)'*virtualspace(exci.left_gs,0)) for k in 1:ham.odim]
+    lBs = typeof(lB_cur)[]
 
     for pos = 1:length(exci)
-        lBs = exci_transfer_left(lBs,ham,pos,exci.right_gs.AR[pos],exci.left_gs.AL[pos])*exp(conj(1im*exci.momentum))
-        lBs += exci_transfer_left(leftenv(lpars,pos,exci.left_gs),ham,pos,exci[pos],exci.left_gs.AL[pos])*exp(conj(1im*exci.momentum))
+        lB_cur = exci_transfer_left(lB_cur,ham,pos,exci.right_gs.AR[pos],exci.left_gs.AL[pos])*exp(conj(1im*exci.momentum))
+        lB_cur += exci_transfer_left(leftenv(lpars,pos,exci.left_gs),ham,pos,exci[pos],exci.left_gs.AL[pos])*exp(conj(1im*exci.momentum))
 
         exci.trivial && for i in ids
-            @tensor lBs[i][-1,-2,-3,-4] -= lBs[i][1,-2,-3,2]*r_RL(exci.left_gs,pos)[2,1]*l_RL(exci.left_gs,pos)[-1,-4]
+            @tensor lB_cur[i][-1,-2,-3,-4] -= lB_cur[i][1,-2,-3,2]*r_RL(exci.left_gs,pos)[2,1]*l_RL(exci.left_gs,pos)[-1,-4]
         end
 
 
-        push!(lBsc,lBs)
+        push!(lBs,lB_cur)
     end
 
     #build rBs(c)
-    rBs = [ TensorMap(zeros,eltype(exci.left_gs),
-                    space(lpars.rw[end,k],1)*space(exci[1],3),
-                    space(rpars.rw[end,k],2)'*space(rpars.rw[end,k],3)') for k in 1:ham.odim]
-    rBsc = typeof(rBs)[]
+    rB_cur = [ TensorMap(zeros,eltype(exci),
+                    virtualspace(exci.right_gs,length(exci))*space(exci[1],3),
+                    ham.imspaces[length(exci),k]*virtualspace(exci.right_gs,length(exci))) for k in 1:ham.odim]
+    rBs = typeof(rB_cur)[]
 
     for pos=length(exci):-1:1
-        rBs = exci_transfer_right(rBs,ham,pos,exci.left_gs.AL[pos],exci.right_gs.AR[pos])*exp(1im*exci.momentum)
-        rBs += exci_transfer_right(rightenv(rpars,pos,exci.right_gs),ham,pos,exci[pos],exci.right_gs.AR[pos])*exp(1im*exci.momentum)
+        rB_cur = exci_transfer_right(rB_cur,ham,pos,exci.left_gs.AL[pos],exci.right_gs.AR[pos])*exp(1im*exci.momentum)
+        rB_cur += exci_transfer_right(rightenv(rpars,pos,exci.right_gs),ham,pos,exci[pos],exci.right_gs.AR[pos])*exp(1im*exci.momentum)
 
         exci.trivial && for i in ids
-            @tensor rBs[i][-1,-2,-3,-4] -= rBs[i][1,-2,-3,2]*l_LR(exci.left_gs,pos)[2,1]*r_LR(exci.left_gs,pos)[-1,-4]
+            @tensor rB_cur[i][-1,-2,-3,-4] -= rB_cur[i][1,-2,-3,2]*l_LR(exci.left_gs,pos)[2,1]*r_LR(exci.left_gs,pos)[-1,-4]
         end
 
-        push!(rBsc,rBs)
+        push!(rBs,rB_cur)
     end
-    rBsc = reverse(rBsc)
+    rBs = reverse(rBs)
 
-    lBsE = left_excitation_transfer_system(lBs,ham,exci)
-    rBsE = right_excitation_transfer_system(rBs,ham,exci)
+    lBE = left_excitation_transfer_system(lB_cur,ham,exci)
+    rBE = right_excitation_transfer_system(rB_cur,ham,exci)
 
-    return InfiniteQPEnv(lBsc,rBsc,lBsE,rBsE,lpars,rpars)
+    lBs[end] = lBE;
+
+    for i=1:length(exci)-1
+        lBE = exci_transfer_left(lBE,ham,i,exci.right_gs.AR[i],exci.left_gs.AL[i])*exp(conj(1im*exci.momentum))
+
+        exci.trivial && for k in ids
+            @tensor lBE[k][-1,-2,-3,-4] -= lBE[k][1,-2,-3,2]*r_RL(exci.left_gs,i)[2,1]*l_RL(exci.left_gs,i)[-1,-4]
+        end
+
+        lBs[i] += lBE;
+    end
+
+    rBs[1] = rBE;
+
+    for i=length(exci):-1:2
+        rBE = exci_transfer_right(rBE,ham,i,exci.left_gs.AL[i],exci.right_gs.AR[i])*exp(1im*exci.momentum)
+
+        exci.trivial && for k in ids
+            @tensor rBE[k][-1,-2,-3,-4]-=rBE[k][1,-2,-3,2]*l_LR(exci.left_gs,i)[2,1]*r_LR(exci.left_gs,i)[-1,-4]
+        end
+
+        rBs[i] += rBE
+    end
+
+    return QPEnv(lBs,rBs,lpars,rpars)
 end
 
 function params(exci::FiniteQP,ham::MPOHamiltonian,lpars=params(exci.left_gs,ham),rpars=exci.trivial ? lpars : params(exci.right_gs,ham))
-    #construct lBsE
-    lBs = [ TensorMap(zeros,eltype(exci),
-                    space(leftenv(lpars,1,exci.left_gs)[k],1)*space(leftenv(lpars,1,exci.left_gs)[k],2),
-                    space(exci[1],3)'*space(leftenv(lpars,1,exci.left_gs)[k],3)') for k in 1:ham.odim]
-    lBsc = typeof(lBs)[]
+    #construct lBE
+    lB_cur = [ TensorMap(zeros,eltype(exci),
+                    virtualspace(exci.left_gs,0)*ham.domspaces[1,k]',
+                    space(exci[1],3)'*virtualspace(exci.left_gs,0)) for k in 1:ham.odim]
+    lBs = typeof(lB_cur)[]
     for pos = 1:length(exci)
-        lBs = exci_transfer_left(lBs,ham,pos,exci.right_gs.AR[pos],exci.left_gs.AL[pos])
-        lBs += exci_transfer_left(leftenv(lpars,pos,exci.left_gs),ham,pos,exci[pos],exci.left_gs.AL[pos])
-        push!(lBsc,lBs)
+        lB_cur = exci_transfer_left(lB_cur,ham,pos,exci.right_gs.AR[pos],exci.left_gs.AL[pos])
+        lB_cur += exci_transfer_left(leftenv(lpars,pos,exci.left_gs),ham,pos,exci[pos],exci.left_gs.AL[pos])
+        push!(lBs,lB_cur)
     end
 
     #build rBs(c)
-    rBs = [ TensorMap(zeros,eltype(exci),
-                    space(rightenv(lpars,length(exci.left_gs),exci.left_gs)[k],1)*space(exci[1],3),
-                    space(rightenv(lpars,length(exci.left_gs),exci.left_gs)[k],2)'
-                    *space(rightenv(lpars,length(exci.left_gs),exci.left_gs)[k],3)') for k in 1:ham.odim]
-    rBsc = typeof(rBs)[]
+    rB_cur = [ TensorMap(zeros,eltype(exci),
+                    virtualspace(exci.right_gs,length(exci))*space(exci[1],3),
+                    ham.imspaces[length(exci),k]*virtualspace(exci.right_gs,length(exci))) for k in 1:ham.odim]
+    rBs = typeof(rB_cur)[]
     for pos=length(exci):-1:1
-        rBs = exci_transfer_right(rBs,ham,pos,exci.left_gs.AL[pos],exci.right_gs.AR[pos])
-        rBs += exci_transfer_right(rightenv(rpars,pos,exci.right_gs),ham,pos,exci[pos],exci.right_gs.AR[pos])
-        push!(rBsc,rBs)
+        rB_cur = exci_transfer_right(rB_cur,ham,pos,exci.left_gs.AL[pos],exci.right_gs.AR[pos])
+        rB_cur += exci_transfer_right(rightenv(rpars,pos,exci.right_gs),ham,pos,exci[pos],exci.right_gs.AR[pos])
+        push!(rBs,rB_cur)
     end
-    rBsc=reverse(rBsc)
+    rBs=reverse(rBs)
 
-    return FiniteQPEnv(lBsc,rBsc,lpars,rpars)
+    return QPEnv(lBs,rBs,lpars,rpars)
 end
