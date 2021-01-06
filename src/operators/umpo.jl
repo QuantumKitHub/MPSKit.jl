29cce40e52cc60280f12680234bff9b58e8081ba
maarten <maarten.vandamme@ugent.be>
2021-01-06 14:18:31 +0100

    repeat periodicmpo

--- src/operators/umpo.jl
+++ src/operators/umpo.jl
@@ -8,6 +8,7 @@ end
 PeriodicMPO(t::AbstractTensorMap) = PeriodicMPO(fill(t,1,1));
 PeriodicMPO(t::AbstractArray{T,2}) where T<:MPOTensor = PeriodicMPO(PeriodicArray(t));
 
+Base.repeat(t::PeriodicMPO,i,j) = PeriodicMPO(repeat(t.opp,i,j));
 Base.getindex(o::PeriodicMPO,i,j) = o.opp[i,j]
 Base.size(o::PeriodicMPO,i) = size(o.opp,i);
 Base.size(o::PeriodicMPO) = size(o.opp);
