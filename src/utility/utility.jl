b54a8f432806438ce054b04acefd5f84f8b5ca27
maarten <maarten.vandamme@ugent.be>
2021-04-22 14:38:38 +0200

    moved into mpskitmodels

--- src/utility/utility.jl
+++ src/utility/utility.jl
@@ -27,65 +27,6 @@ end
 _firstspace(t::AbstractTensorMap) = space(t, 1)
 _lastspace(t::AbstractTensorMap) = space(t, numind(t))
 
-"
-    Returns spin operators Sx,Sy,Sz,Id for spin s
-"
-function spinmatrices(s::Union{Rational{Int},Int})
-    N = Int(2*s)
-
-    Sx=zeros(Defaults.eltype,N+1,N+1)
-    Sy=zeros(Defaults.eltype,N+1,N+1)
-    Sz=zeros(Defaults.eltype,N+1,N+1)
-
-    for row=1:(N+1)
-        for col=1:(N+1)
-            term=sqrt((s+1)*(row+col-1)-row*col)/2.0
-
-            if (row+1==col)
-                Sx[row,col]+=term
-                Sy[row,col]-=1im*term
-            end
-
-            if(row==col+1)
-                Sx[row,col]+=term
-                Sy[row,col]+=1im*term
-            end
-
-            if(row==col)
-                Sz[row,col]+=s+1-row
-            end
-
-        end
-    end
-    return Sx,Sy,Sz,one(Sx)
-end
-
-function nonsym_spintensors(s)
-    (Sxd,Syd,Szd) = spinmatrices(s)
-    sp = ComplexSpace(size(Sxd,1))
-
-    Sx = TensorMap(Sxd,sp,sp);
-    Sy = TensorMap(Syd,sp,sp);
-    Sz = TensorMap(Szd,sp,sp);
-
-    return Sx,Sy,Sz,one(Sx)
-end
-
-"""
-bosonic creation anihilation operators with a cutoff
-cutoff = maximal number of bosons at one location
-"""
-function nonsym_bosonictensors(cutoff::Int)
-    creadat = zeros(Defaults.eltype,cutoff+1,cutoff+1);
-
-    for i in 1:cutoff
-        creadat[i+1,i] = sqrt(i);
-    end
-
-    a⁺ = TensorMap(creadat,ℂ^(cutoff+1),ℂ^(cutoff+1));
-    a⁻ = TensorMap(collect(creadat'),ℂ^(cutoff+1),ℂ^(cutoff+1));
-    return (a⁺,a⁻)
-end
 #given a hamiltonian with unit legs on the side, decompose it using svds to form a "localmpo"
 function decompose_localmpo(inpmpo::AbstractTensorMap{PS,N1,N2},trunc = truncbelow(Defaults.tol)) where {PS,N1,N2}
     numind=N1+N2
@@ -101,8 +42,6 @@ function decompose_localmpo(inpmpo::AbstractTensorMap{PS,N1,N2},trunc = truncbel
 end
 
 function add_util_leg(tensor::AbstractTensorMap{S,N1,N2}) where {S,N1,N2}
-    #ntuple(x->x,Val{3+4}())
-
     util=Tensor(ones,eltype(tensor),oneunit(space(tensor,1)))
     tensor1=util*permute(tensor,(),ntuple(x->x,Val{N1+N2}()))
     return permute(tensor1,ntuple(x->x,Val{N1+N2+1}()),())*util'
