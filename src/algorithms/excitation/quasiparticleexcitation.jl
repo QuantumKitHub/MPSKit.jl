5bfc5aebb7f52b5cea287ff757dd981e500a5c99
lkdvos <lukas.devos@ugent.be>
2021-06-18 18:14:49 +0200

    Allow using initial QP for `excitations`

--- src/algorithms/excitation/quasiparticleexcitation.jl
+++ src/algorithms/excitation/quasiparticleexcitation.jl
@@ -10,58 +10,151 @@ end
 
 include("excitransfers.jl")
 
-"
-    quasiparticle_excitation calculates the energy of the first excited state at momentum 'moment'
-"
-function excitations(H::Hamiltonian, alg::QuasiparticleAnsatz, momentum::Float64, mpsleft::InfiniteMPS, environmentsleft=environments(mpsleft,hamiltonian), mpsright::InfiniteMPS=mpsleft, environmentsright=environmentsleft; sector=first(sectors(oneunit(virtualspace(mpsleft, 1)))), num=1, solver=Defaults.solver)
+"""
+    excitations(H::Hamiltonian, alg::QuasiparticleAnsatz, args...; kwargs...)
 
-    V_initial = LeftGaugedQP(rand, mpsleft, mpsright; sector=sector, momentum=momentum);
+Compute the first excited states and their energy gap above a groundstate.
 
-    #the function that maps x->B and then places this in the excitation hamiltonian
-    eigEx = @closure(V->effective_excitation_hamiltonian(H, V, environments(V, H, environmentsleft, environmentsright, solver=solver)))
-    Es, Vs, convhist = eigsolve(eigEx, V_initial, num, :SR, tol=alg.toler,krylovdim=alg.krylovdim)
-    convhist.converged < num && @warn "quasiparticle didn't converge k=$(momentum) $(convhist.normres)"
+This is an implementation of the algorithm found [here](https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.111.080401).
+"""
+function excitations end
 
+################################################################################
+#                           Infinite Excitations                               #
+################################################################################
+"""
+    excitations(H, alg, V₀::InfiniteQP, lenvs, renvs; num, solver)
+
+Optimise the infinite quasiparticle state ```V₀```.
+
+# Arguments
+- `V₀::InfiniteQP`: initial quasiparticle state.
+- `lenvs=environments(V₀.left_gs, H; solver=solver)`: left environment of the groundstate.
+- `renvs=(V₀.trivial ? lenvs : environments(V₀.right_gs, H; solver=solver))`: right environment of the groundstate.
+- `num=1`: number of excited states to compute.
+- `solver=Defaults.solver`: the algorithm to compute the environments.
+"""
+function excitations(
+    H::Hamiltonian, alg::QuasiparticleAnsatz, V₀::InfiniteQP, lenvs, renvs;
+    num=1, solver=Defaults.solver
+)
+    qp_envs(V) = environments(V, H, lenvs, renvs, solver=solver)
+    H_eff = @closure(V->effective_excitation_hamiltonian(H, V, qp_envs(V)))
+    
+    Es, Vs, convhist = eigsolve(
+        H_eff, V₀, num, :SR, tol=alg.toler, krylovdim=alg.krylovdim
+    )
+    
+    convhist.converged < num && 
+        @warn "Quasiparticle didn't converge: $(convhist.normres)"
+    
     return Es, Vs
 end
-
-#pretty much identical to the infinite mps code, except for the lack of momentum label
-function excitations(H::Hamiltonian, alg::QuasiparticleAnsatz, mpsleft::FiniteMPS, environmentsleft=environments(mpsleft, H), mpsright::FiniteMPS=mpsleft, environmentsright=environmentsleft;
-    sector=first(sectors(oneunit(virtualspace(mpsleft, 1)))), num=1)
-
-    V_initial = LeftGaugedQP(rand, mpsleft, mpsright; sector=sector);
-
-    #the function that maps x->B and then places this in the excitation hamiltonian
-    eigEx = @closure(V->effective_excitation_hamiltonian(H, V, environments(V, H, environmentsleft, environmentsright)))
-    Es, Vs, convhist = eigsolve(eigEx, V_initial, num, :SR, tol=alg.toler,krylovdim=alg.krylovdim)
-    convhist.converged < num && @warn "quasiparticle didn't converge $(convhist.normres)"
-
-    return Es, Vs
+function excitations(
+    H::Hamiltonian, alg::QuasiparticleAnsatz, V₀::InfiniteQP, lenvs;
+    num=1, solver=Defaults.solver
+)
+    # Infer `renvs` in function body as it depends on `solver`.
+    renvs = V₀.trivial ? lenvs : environments(V₀.right_gs, H, solver=solver)
+    return excitations(H, alg, V₀, lenvs, renvs; num, solver)
+end
+function excitations(
+    H::Hamiltonian, alg::QuasiparticleAnsatz, V₀::InfiniteQP;
+    num=1, solver=Defaults.solver
+)
+    # Infer `lenvs` in function body as it depends on `solver`.
+    lenvs = environments(V₀.left_gs, H, solver=solver)
+    return excitations(H, alg, V₀, lenvs; num, solver)
 end
 
-#give it a vector of momentum points
-function excitations(H::Hamiltonian, alg::QuasiparticleAnsatz, momenta::AbstractVector, mpsleft::InfiniteMPS, environmentsleft=environments(mpsleft, H), mpsright::InfiniteMPS=mpsleft, environmentsright=environmentsleft;
-    num=1, verbose=Defaults.verbose, kwargs...)
+"""
+    excitations(H, alg, p::Real, lmps, lenvs, rmps, renvs; sector, kwargs...)
 
-    tasks = map(enumerate(momenta)) do (i, p)
+Create and optimise an infinite quasiparticle state with momentum ```p```.
+"""
+function excitations(
+    H::Hamiltonian, alg::QuasiparticleAnsatz, p::Real, 
+    lmps::InfiniteMPS, lenvs=environments(lmps, H),
+    rmps::InfiniteMPS=lmps, renvs=lmps===rmps ? lenvs : environments(rmps, H);
+    sector=first(sectors(oneunit(virtualspace(lmps, 1)))), num=1,
+    solver=Defaults.solver
+)
+    V₀ = LeftGaugedQP(rand, lmps, rmps; sector=sector, momentum=p)
+    return excitations(H, alg, V₀, lenvs, renvs; num=num, solver=solver)
+end
+function excitations(
+    H::Hamiltonian, alg::QuasiparticleAnsatz, momenta::AbstractVector,
+    lmps::InfiniteMPS, lenvs=environments(lmps, H),
+    rmps::InfiniteMPS=lmps, renvs=lmps===rmps ? lenvs : environments(rmps, H);
+    verbose=Defaults.verbose, kwargs...
+)
+    tasks = map(momenta) do p
         @Threads.spawn begin
-            (E, V) = excitations(H, alg, p, mpsleft, environmentsleft, mpsright, environmentsright; num=num, kwargs...)
+            (E, V) = excitations(H, alg, p, lmps, lenvs, rmps, renvs; kwargs...)
             verbose && @info "Found excitations for p = $(p)"
             (E, V)
         end
     end
-
+    
     fetched = fetch.(tasks);
-
+    
     Ep = permutedims(reduce(hcat, map(x->x[1][1:num], fetched)));
     Bp = permutedims(reduce(hcat, map(x->x[2][1:num], fetched)));
-
-    return Ep,Bp
+    
+    return Ep, Bp
 end
 
-function effective_excitation_hamiltonian(H::MPOHamiltonian, exci::QP,envs=environments(exci, H))
-    odim = H.odim;
+################################################################################
+#                           Finite Excitations                                 #
+################################################################################
+"""
+    excitations(H, alg, V₀::FiniteQP[, lenvs[, renvs]]; num)
 
+Optimise the finite quasiparticle state ```V₀```.
+
+# Arguments
+- `V₀::FiniteQP`: initial quasiparticle state.
+- `lenvs=environments(V₀.left_gs, H)`: left environment of the groundstate.
+- `renvs=(V₀.trivial ? lenvs : environments(V₀.right_gs, H))`: right environment of the groundstate.
+- `num=1`: number of excited states to compute.
+"""
+function excitations(
+    H::Hamiltonian, alg::QuasiparticleAnsatz, 
+    V₀::FiniteQP, lenvs=environments(V₀.left_gs, H),
+    renvs=V₀.trivial ? lenvs : environments(V₀.right_gs, H);
+    num=1
+)
+    qp_envs(V) = environments(V, H, lenvs, renvs)
+    H_eff = @closure(V->effective_excitation_hamiltonian(H, V, qp_envs(V)))
+    
+    Es, Vs, convhist = eigsolve(
+        H_eff, V₀, num, :SR, tol=alg.toler, krylovdim=alg.krylovdim
+    )
+    
+    convhist.converged < num &&
+        @warn "Quasiparticle didn't converge: $(convhist.normres)"
+    
+    return Es, Vs
+end
+
+"""
+    excitations(H, alg, [lmps, [lenvs, [rmps, [renvs]]]]; sector, kwargs...)
+
+Create and optimise a finite quasiparticle state.
+"""
+function excitations(
+    H::Hamiltonian, alg::QuasiparticleAnsatz, 
+    lmps::FiniteMPS, lenvs=environments(lmps, H),
+    rmps::FiniteMPS=lmps, renvs=lmps===rmps ? lenvs : environments(rmps, H)environmentsleft;
+    sector=first(sectors(oneunit(virtualspace(lmps, 1)))), num=1
+)
+    V₀ = LeftGaugedQP(rand, lmps, rmps; sector=sector);
+    return excitations(H, alg, V₀, lenvs, renvs; num=num)
+end
+
+function effective_excitation_hamiltonian(
+    H::MPOHamiltonian, exci::QP,envs=environments(exci, H)
+)
     Bs = [exci[i] for i in 1:length(exci)];
     toret = similar(exci);
 
