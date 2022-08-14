7642c99e6f669d590f583b19e228975589e2ec90
maarten <maarten.vandamme@ugent.be>
2022-08-14 10:17:06 +0200

    add a test for the idmrg space mismatch

--- test/runtests.jl
+++ test/runtests.jl
@@ -340,7 +340,7 @@ end
             @test variance(Bs[1],th)<1e-8
 
             #find energy with normal dmrg
-            (energies_dm,_) = excitations(th,FiniteExcited(gsalg=DMRG(verbose=false)),ts);
+            (energies_dm,_) = excitations(th,FiniteExcited(gsalg=DMRG(verbose=false,tol=1e-6)),ts);
             @test energies_dm[1] ≈ energies_QP[1]+sum(expectation_value(ts,th,envs)) atol=1e-4
 
             energies_QP[1]
@@ -602,4 +602,35 @@ end
     ts = InfiniteMPS([𝔹^2],[𝔹^5]);
     @test transferplot(ts) isa Plots.Plot
     @test entanglementplot(ts) isa Plots.Plot
+end
+
+
+println("------------------------------------")
+println("|     Old bugs                     |")
+println("------------------------------------")
+
+@timedtestset "IDMRG2 space mismatch" begin
+    ## Hamiltonian ##
+
+    function heisenberg_interaction(spin)
+        physical = Rep[SU₂](spin => 1)
+        adjoint = Rep[SU₂](1 => 1)
+
+        Sl = TensorMap(ones, ComplexF64, physical, adjoint ⊗ physical)
+        Sr = TensorMap(ones, ComplexF64, adjoint ⊗ physical, physical)
+
+        return @tensor H[-1 -2; -3 -4] := Sl[-1; 1 -3] * Sr[1 -2; -4] * (spin * (spin + 1))
+    end
+
+    H_heis = heisenberg_interaction(1);
+    @tensor H_aklt[-1 -2; -3 -4] := H_heis[-1 -2; -3 -4] + 1/3 * H_heis[-1 -2; 1 2] * H_heis[1 2; -3 -4]
+    ##
+    H = MPOHamiltonian(H_aklt);
+    N = 6
+    H = repeat(H, N);
+    ψ₀ = InfiniteMPS(fill(SU2Space(1 => 1), N), fill(SU2Space(1//2 => 2, 3//2 => 1), N));
+    alg = IDMRG2(; tol_galerkin=1e-5, trscheme = truncdim(32));
+
+    ψ, envs, δ = find_groundstate(ψ₀, H, alg); # used to error
+    @test ψ isa InfiniteMPS
 end
