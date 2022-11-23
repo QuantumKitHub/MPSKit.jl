6f9089291859a24c15babd69fdcd05895b19e8c8
maarten <maarten.vandamme@ugent.be>
2022-11-23 09:29:40 +0100

    does this unbreak docs?

--- src/algorithms/correlators.jl
+++ src/algorithms/correlators.jl
@@ -1,8 +1,8 @@
 """
-    correlator(ψ, O₁, O₂, i, j)
-    correlator(ψ, O₁₂, i, j)
+    correlator(ψ, O1, O2, i, j)
+    correlator(ψ, O12, i, j)
 
-Compute the 2-point correlator <ψ|O₁[i]O₂[j]|ψ> for inserting `O₁` at `i` and `O₂` at `j`.
+Compute the 2-point correlator <ψ|O1[i]O2[j]|ψ> for inserting `O1` at `i` and `O2` at `j`.
 Also accepts ranges for `j`.
 """
 function correlator end
