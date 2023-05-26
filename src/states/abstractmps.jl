b01dd3fe9e9e41b92c77682887df0e90dcdd654b
lkdvos <lukas.devos@ugent.be>
2023-05-26 15:43:24 +0200

    Remove EuclideanSpace

--- src/states/abstractmps.jl
+++ src/states/abstractmps.jl
@@ -2,10 +2,10 @@
 Tensor types
 ===========================================================================================#
 
-const MPOTensor{S} = AbstractTensorMap{S,2,2} where {S<:EuclideanSpace}
-const MPSBondTensor{S} = AbstractTensorMap{S,1,1} where {S<:EuclideanSpace}
-const GenericMPSTensor{S,N} = AbstractTensorMap{S,N,1} where {S<:EuclideanSpace,N} #some functions are also defined for "general mps tensors" (used in peps code)
-const MPSTensor{S} = GenericMPSTensor{S,2} where {S<:EuclideanSpace} #the usual mps tensors on which we work
+const MPOTensor{S} = AbstractTensorMap{S,2,2} where {S}
+const MPSBondTensor{S} = AbstractTensorMap{S,1,1} where {S}
+const GenericMPSTensor{S,N} = AbstractTensorMap{S,N,1} where {S,N} #some functions are also defined for "general mps tensors" (used in peps code)
+const MPSTensor{S} = GenericMPSTensor{S,2} where {S} #the usual mps tensors on which we work
 #const ExMPSTensor{S,N,A,G,F1,F2}=GenericMPSTensor{S,3,A,G,F1,F2} #and mps tensor with an extra excitation - utility leg
 
 """
