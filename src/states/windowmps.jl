550a9a72c9fdc3bcd9c7fb456cfbda2645539629
lkdvos <lukas.devos@ugent.be>
2023-06-22 09:19:19 +0200

    Update docstring of WindowMPS

--- src/states/windowmps.jl
+++ src/states/windowmps.jl
@@ -1,8 +1,36 @@
-"
-    WindowMPS(leftstate,window,rightstate)
+"""
+    WindowMPS{A<:GenericMPSTensor,B<:MPSBondTensor} <: AbstractFiniteMPS
 
-    mutable window of tensors on top of an infinite chain
-"
+Type that represents a finite Matrix Product State embedded in an infinte Matrix Product State.
+
+## Fields
+
+- `left_gs::InfiniteMPS` -- left infinite environment
+- `window::FiniteMPS` -- finite window Matrix Product State
+- `right_gs::InfiniteMPS` -- right infinite environment
+
+---
+
+## Constructors
+
+    WindowMPS(left_gs::InfiniteMPS, window_state::FiniteMPS, [right_gs::InfiniteMPS])
+    WindowMPS(left_gs::InfiniteMPS, window_tensors::AbstractVector, [right_gs::InfiniteMPS])
+    WindowMPS([f, eltype], physicalspaces::Vector{<:Union{S, CompositeSpace{S}},
+              virtualspaces::Vector{<:Union{S, CompositeSpace{S}}, left_gs::InfiniteMPS,
+              [right_gs::InfiniteMPS])
+    WindowMPS([f, eltype], physicalspaces::Vector{<:Union{S,CompositeSpace{S}}},
+              maxvirtualspace::S, left_gs::InfiniteMPS, [right_gs::InfiniteMPS])
+    
+Construct a WindowMPS via a specification of left and right infinite environment, and either
+a window state or a vector of tensors to construct the window. Alternatively, it is possible
+to supply the same arguments as for the constructor of [`FiniteMPS`](@ref), followed by a
+left (and right) environment to construct the WindowMPS in one step.
+
+    WindowMPS(state::InfiniteMPS, L::Int)
+
+Construct a WindowMPS from an InfiniteMPS, by promoting a region of length `L` to a
+`FiniteMPS`.
+"""
 mutable struct WindowMPS{A<:GenericMPSTensor,B<:MPSBondTensor} <: AbstractFiniteMPS
     left_gs::InfiniteMPS{A,B}
     window::FiniteMPS{A,B}
