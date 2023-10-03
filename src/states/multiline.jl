d860174be7c25e200dcdaa1a889c3f0ba1075ce2
lkdvos <lukas.devos@ugent.be>
2023-10-03 22:55:54 +0200

    Rewrite multiline as AbstractVector

--- src/states/multiline.jl
+++ src/states/multiline.jl
@@ -3,52 +3,75 @@ It is possible to have matrix product (operators / states) that are also periodi
 For examples, as fix points of statmech problems
 These should be represented as respectively MultiLine{<:DenseMPO} / Multiline{<:InfiniteMPS}
 "
-struct Multiline{T}
+
+"""
+    struct Multiline{T} <: AbstractVector{T}
+
+Object that represents multiple lines of objects of type `T`. Typically used to represent
+multiple lines of `InfiniteMPS` (`MPSMultiline`) or MPO (`Multiline{<:AbstractMPO}`).
+
+# Fields
+- `data::PeriodicArray{T,1}`: the data of the multiline object
+
+See also: [`MPSMultiline`](@ref)
+"""
+struct Multiline{T} <: AbstractVector{T}
     data::PeriodicArray{T,1}
 end
 
-Base.length(t::Multiline) = prod(size(t));
-Base.size(t::Multiline) = (length(t.data), length(t.data[1]));
-Base.size(t::Multiline, i) = size(t)[i];
-Base.getindex(t::Multiline, i) = t.data[i];
-Base.copy(t::Multiline) = Multiline(map(copy, t.data));
-Multiline(t::AbstractArray) = Multiline(PeriodicArray(t));
-Base.iterate(t::Multiline, args...) = iterate(t.data, args...);
-Base.convert(::Vector, t::Multiline) = t.data.data;
-Base.convert(::PeriodicArray, t::Multiline) = t.data;
+Multiline(data::AbstractVector{T}) where {T} = Multiline{T}(data)
 
-Base.convert(::Multiline, v::AbstractArray) = Multiline(v);
+# AbstractArray interface
+# -----------------------
+Base.parent(m::Multiline) = m.data
+Base.size(m) = size(parent(m))
+Base.getindex(m, i::Int) = parent(m)[i]
+Base.setindex!(m, v, i::Int) = (setindex!(parent(m), v, i); m)
 
-#--- implementation of MPSMultiline
-const MPSMultiline = Multiline{<:InfiniteMPS}
+Base.copy(t::Multiline) = Multiline(map(copy, t.data))
+# Multiline(t::AbstractArray) = Multiline(PeriodicArray(t));
+# Base.iterate(t::Multiline, args...) = iterate(t.data, args...);
 
-function MPSMultiline(
-    pspaces::AbstractArray{S,2}, Dspaces::AbstractArray{S,2}; kwargs...
-) where {S}
-    return MPSMultiline(
-        map(zip(circshift(Dspaces, (0, -1)), pspaces, Dspaces)) do (D1, p, D2)
-            TensorMap(rand, Defaults.eltype, D1 * p, D2)
-        end;
-        kwargs...,
-    )
+# MPSMultiline
+# ------------
+"""
+    const MPSMultiline{T} = Multiline{<:InfiniteMPS}
+
+Type that represents multiple lines of `InfiniteMPS` objects.
+
+# Constructors
+    MPSMultiline([f, eltype], physicalspaces::Matrix{<:Union{S, CompositeSpace{S}},
+                 virtualspaces::Matrix{<:Union{S, CompositeSpace{S}}) where
+                 {S<:ElementarySpace}
+    MPSMultiline(As::AbstractMatrix{<:GenericMPSTensor}; kwargs...)
+    MPSMultiline(ALs::AbstractMatrix{<:GenericMPSTensor}, 
+                 C₀::AbstractVector{<:MPSBondTensor}; kwargs...)
+
+See also: [`Multiline`](@ref)
+"""
+const MPSMultiline{A,B} = Multiline{InfiniteMPS{A,B}}
+
+function MPSMultiline(pspaces::AbstractMatrix{S}, Dspaces::AbstractMatrix{S}; kwargs...) where {S}
+    data = map(eachrow(pspaces), eachrow(Dspaces)) do (p, D)
+        return InfiniteMPS(p, D; kwargs...)
+    end
+    return MPSMultiline(data)
+end
+function MPSMultiline(As::AbstractMatrix{T}; kwargs...) where {T<:GenericMPSTensor}
+    data = map(eachrow(As)) do Arow
+        return InfiniteMPS(Arow; kwargs...)
+    end
+    return MPSMultiline(data)
+end
+function MPSMultiline(ALs::AbstractMatrix{<:GenericMPSTensor}, 
+                      C₀::AbstractVector{<:MPSBondTensor}; kwargs...)
+    data = map(eachrow(ALs), C₀) do (ALrow, C₀row)
+        return InfiniteMPS(ALrow, C₀row; kwargs...)
+    end
+    return MPSMultiline(data)
 end
 
-function MPSMultiline(data::AbstractArray{T,2}; kwargs...) where {T<:GenericMPSTensor}
-    return Multiline(PeriodicArray(
-        map(1:size(data, 1)) do row
-            InfiniteMPS(data[row, :]; kwargs...)
-        end,
-    ))
-end
-function MPSMultiline(
-    data::AbstractArray{T,2}, Cinit::AbstractArray{O,1}; kwargs...
-) where {T<:GenericMPSTensor,O<:MPSBondTensor}
-    return Multiline(PeriodicArray(
-        map(1:size(data, 1)) do row
-            InfiniteMPS(data[row, :], Cinit[row]; kwargs...)
-        end,
-    ))
-end
+# TODO: properly rewrite these properties
 function Base.getproperty(psi::MPSMultiline, prop::Symbol)
     if prop == :AL
         return ALView(psi)
@@ -71,17 +94,17 @@ for f in (:r_RR, :r_RL, :r_LR, :r_LL)
     @eval $f(t::MPSMultiline, i, j=size(t, 2)) = $f(t[i], j)
 end
 
-site_type(::Type{Multiline{S}}) where {S} = site_type(S);
-bond_type(::Type{Multiline{S}}) where {S} = bond_type(S);
+site_type(::Type{Multiline{S}}) where {S} = site_type(S)
+bond_type(::Type{Multiline{S}}) where {S} = bond_type(S)
 site_type(st::Multiline) = site_type(typeof(st))
 bond_type(st::Multiline) = bond_type(typeof(st))
 
 function TensorKit.dot(a::MPSMultiline, b::MPSMultiline; kwargs...)
-    return sum(dot.(a.data, b.data; kwargs...))
+    return sum(dot.(parent(a), parent(b); kwargs...))
 end
 
-Base.convert(::Type{MPSMultiline}, st::InfiniteMPS) = Multiline([st]);
-Base.convert(::Type{InfiniteMPS}, st::MPSMultiline) = st[1];
-Base.eltype(t::MPSMultiline) = eltype(t[1]);
+Base.convert(::Type{MPSMultiline}, st::InfiniteMPS) = Multiline([st])
+Base.convert(::Type{InfiniteMPS}, st::MPSMultiline) = only(st)
+# Base.eltype(t::MPSMultiline) = eltype(t[1]);
 left_virtualspace(t::MPSMultiline, i::Int, j::Int) = left_virtualspace(t[i], j);
 right_virtualspace(t::MPSMultiline, i::Int, j::Int) = right_virtualspace(t[i], j);
