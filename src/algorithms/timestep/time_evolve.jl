3c9ffb35aa86abc763f5ee98cc9f39e0a58be2b0
lkdvos <lukas.devos@ugent.be>
2023-10-10 18:36:52 +0200

    Add `time_evolve` implementation

--- src/algorithms/timestep/time_evolve.jl
+++ src/algorithms/timestep/time_evolve.jl
@@ -14,16 +14,26 @@ through each of the time points obtained by iterating t_span.
 """
 function time_evolve end, function time_evolve! end
 
-# re-route finite MPS to in-place version
-function time_evolve(Ψ₀::AbstractFiniteMPS, args...; kwargs...)
-    return time_evolve!(copy(Ψ₀), args...; kwargs...)
-end
-
-function time_evolve!(Ψ₀::AbstractFiniteMPS, H, t_span::AbstractVector{<:Number}, alg, envs=environments(Ψ₀, H); verbose=false)
-    for (dt, t) in zip(diff(t_span), t_span[2:end])
-        elapsed = @elapsed Ψ₀, envs = timestep!(Ψ₀, H, t, dt, alg, envs)
-        verbose && @info "Timestep to $t completed" elapsed
+# TODO: is it possible to remove this code-duplication?
+function time_evolve(
+    ψ, H, t_span::AbstractVector{<:Number}, alg, envs=environments(Ψ₀, H); verbose=false
+)
+    for (t, dt) in zip(t_span[2:end], diff(t_span))
+        elapsed = @elapsed ψ, envs = timestep(ψ, H, t, dt, alg, envs)
+        verbose && @info "Timestep iteration:" t elapsed
+        ψ, envs = alg.finalize(t, ψ, H, envs)::Tuple{typeof(ψ),typeof(envs)}
     end
+    return ψ, envs
+end
+function time_evolve!(
+    ψ, H, t_span::AbstractVector{<:Number}, alg, envs=environments(Ψ₀, H); verbose=false
+)
+    for (t, dt) in zip(t_span[2:end], diff(t_span))
+        elapsed = @elapsed ψ, envs = timestep!(ψ, H, t, dt, alg, envs)
+        verbose && @info "Timestep iteration:" t elapsed
+        ψ, envs = alg.finalize(t, ψ, H, envs)::Tuple{typeof(ψ),typeof(envs)}
+    end
+    return ψ, envs
 end
 
 """
@@ -32,7 +42,6 @@ end
 
 Time-step the state `ψ₀` with Hamiltonian `H` over a given time step `dt` at time `t`,
 solving the Schroedinger equation: ``i ∂ψ/∂t = H ψ``.
-
 
 # Arguments
 - `ψ₀::AbstractMPS`: initial state
@@ -43,125 +52,3 @@ solving the Schroedinger equation: ``i ∂ψ/∂t = H ψ``.
 - `[envs]`: MPS environment manager
 """
 function timestep end, function timestep! end
-
-
-"""
-    SimpleScheme{F} <: Algorithm
-
-The simplest numerical scheme to do time evolution by doing consecutive individual timesteps.
-
-# Fields
-- `stepalg::S`: algorithm used to do the individual timesteps
-- `ts`::AbstractVector: time step points
-- `dts`::AbstractVector: dt of each time step
-- `time_finalize::F`: user-supplied function which is applied after each iteration which can be used to save objects during the time evolution, with
-    signature `finalize(iter, Ψ, H, envs) -> Ψ, envs, saved`
-
-"""
-struct SimpleScheme{S,F,N} <: MPSKit.Algorithm
-    stepalg::S
-    ts::AbstractVector{N}
-    dts::AbstractVector{N}
-    time_finalize::F
-
-    function SimpleScheme(
-        stepalg::S, ts::AbstractVector{N}, dts::AbstractVector{N}, time_finalize::F
-    ) where {N<:Number,S,F}
-        length(ts) == length(dts) + 1 ||
-            throw(ArgumentError("times and timesteps length need to be compatible"))
-        all(isapprox.(test.ts[1:(end - 1)] .+ test.dts, test.ts[2:end])) ||
-            throw(ArgumentError("times and timesteps need to be compatible"))
-        return new{S,F,N}(stepalg, ts, dts, time_finalize)
-    end
-end
-function SimpleScheme(stepalg, ts::AbstractVector, time_finalize)
-    return SimpleScheme(stepalg, ts, ts[2:end] .- ts[1:(end - 1)], time_finalize)
-end
-
-function SimpleScheme(stepalg, ts::AbstractRange, time_finalize)
-    return SimpleScheme(stepalg, ts, repeat([step(ts)], length(ts) - 1), time_finalize)
-end
-
-#implement iteration
-function Base.iterate(x::SimpleScheme, state=1)
-    if length(x.ts) == 0 || state == length(x.ts)
-        return nothing
-    else
-        return ((x.ts[state], x.dts[state]), state + 1)
-    end
-end
-
-"""
-    time_evolve(Ψ, H, tspan, [environments]; kwargs...)
-    time_evolve(Ψ, H, scheme, [environments]; kwargs...)
-
-Time evolve the initial state `Ψ` with Hamiltonian `H` over a time span `tspan`. If not specified the
-time step `scheme` defaults to `SimpleScheme` which is just a consecutive iteration over `tspan`.
-If not specified, an algorithm for the individual time steps will be attempted based on the supplied keywords.
-
-## Arguments
-- `Ψ::AbstractMPS`: initial state
-- `H::AbstractMPO`: operator that generates the time evolution (can be time-dependent).
-- `[environments]`: MPS environment manager
-- `tspan::AbstractVector`: time points over which the time evolution is stepped
-- `scheme`: time step scheme
-
-## Keywords
-- `tol::Float64`: tolerance for convergence criterium
-- `maxiter::Int`: maximum amount of iterations
-- `verbose::Bool`: display progress information
-"""
-function time_evolve!(
-    Ψ,
-    H,
-    tspan::AbstractVector{<:Number},
-    envs::Cache=environments(Ψ, H);
-    integrator=Lanczos(; tol=tol),
-    tol=Defaults.tol,
-    tolgauge=Defaults.tolgauge,
-    gaugemaxiter=Defaults.maxiter,
-    verbose=Defaults.verbose,
-    trscheme=nothing,
-    stepalg=TDVP(;
-        integrator=integrator,
-        tol=tol,
-        tolgauge=tolgauge,
-        gaugemaxiter=gaugemaxiter,
-        verbose=verbose,
-    ),
-    time_finalize=Defaults._time_finalize,
-    saved=[],
-)
-    if !isnothing(trscheme)
-        stepalg = TDVP2(;
-            integrator=integrator,
-            tol=tol,
-            tolgauge=tolgauge,
-            gaugemaxiter=gaugemaxiter,
-            verbose=verbose,
-            trscheme=trscheme,
-        )
-    end
-    scheme = SimpleScheme(stepalg, tspan, time_finalize)
-    return time_evolve!(Ψ, H, scheme, envs; saved=saved)
-end
-
-function time_evolve(Ψ, H, tspan, envs::Cache=environments(Ψ, H); kwargs...)
-    return time_evolve(copy(Ψ), H, tspan, envs; kwargs...)
-end
-
-function time_evolve!(Ψ, H, scheme::SimpleTimeScheme, envs=environments(Ψ, H); saved=[])
-    _, _, tobesaved = alg.finalize(1, Ψ, H, envs)
-    isnothing(tobesaved) || push!(saved, tobesaved)
-    for (iter, (t, dt)) in enumerate(scheme)
-        Ψ, envs = timestep!(Ψ, H, t, dt, scheme.stepalg, envs)
-
-        Ψ, envs, tobesaved =
-            scheme.finalize(iter, Ψ, H, envs)::Tuple{typeof(Ψ),typeof(envs),eltype(saved)}
-        isnothing(tobesaved) || push!(saved, tobesaved)
-    end
-    return Ψ, envs, saved
-end
-function time_evolve(Ψ, H, scheme, envs::Cache=environments(Ψ, H); kwargs...)
-    return time_evolve!(copy(Ψ), H, scheme, envs; kwargs...)
-end
