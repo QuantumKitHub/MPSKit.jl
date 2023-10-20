9d7eb2c4b945a3c22aa2fb6b3e443d2aadd3bcfb
DaanMaertens <daan.maertens@gmail.com>
2023-10-20 14:24:41 +0200

    integrate now does i d/dt  = f(y,t)

--- src/algorithms/timestep/integrators.jl
+++ src/algorithms/timestep/integrators.jl
@@ -1,7 +1,7 @@
 """
     integrate(f, y₀, t, dt, alg)
 
-Integrate the differential equation ``dy/dt = f(y, t)`` over a time step 'dt' starting from
+Integrate the differential equation ``i dy/dt = f(y, t)`` over a time step 'dt' starting from
 ``y(t₀)=y₀``, using the provided algorithm.
 
 # Arguments
@@ -14,20 +14,11 @@ Integrate the differential equation ``dy/dt = f(y, t)`` over a time step 'dt' st
 function integrate end
 
 # default for things that are callable on two arguments
-_eval_t(f, t::Number) = Base.Fix2(f, t)
-_eval_x(f, x) = Base.Fix1(f, x)
-
-# TODO: properly clean up this mess
-const DerivativeOperator = Union{MPO_∂∂C,MPO_∂∂AC,MPO_∂∂AC2}
-(h::DerivativeOperator)(x, ::Number) = h(x)
-
-_eval_t(h::DerivativeOperator, t::Number) = h
-_eval_x(h::DerivativeOperator, x) = t -> h(x)
-
-# _eval_x(h::SumOfOperators, x) = h(x)
+_eval_t(f::DerivativeOperator, t::Number) = Base.Fix2(f, t)
+_eval_x(f::DerivativeOperator, x)         = Base.Fix1(f, x)
 
 function integrate(f, y₀, t::Number, dt::Number, alg::Union{Arnoldi,Lanczos})
-    y, convhist = exponentiate(_eval_t(f, t), dt, y₀, alg)
+    y, convhist = exponentiate(_eval_t(f, t), -1im*dt, y₀, alg)
     convhist.converged == 0 && @warn "integration failed $(convhist.normres)"
     return y
 end
