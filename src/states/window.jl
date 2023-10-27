ff3bf422fa1e70ce539499d77844690759112b07
DaanMaertens <daan.maertens@gmail.com>
2023-10-27 17:20:26 +0200

    callable window

--- src/states/window.jl
+++ src/states/window.jl
@@ -13,3 +13,8 @@ end
 
 # do we need copy? 
 # Base.copy(win::Window) = Window(copy(win.left),copy(win.middle),copy(win.right))
+
+# need to define a window(t) too
+#(x::Window{L,M,R})(t::Number) where {T<:TimedOperator,L<:Union{T,SumOfOperators{T}},M<:Union{T,SumOfOperators{T}},R<:Union{T,SumOfOperators{T}}} = Window(x.left(t),x.middle(t),x.right(t))
+
+(x::Window)(t::Number) = Window(x.left(t),x.middle(t),x.right(t))
