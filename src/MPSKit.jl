fa4adc354a41c0f3ddee7375251d92ea9ae6bad5
DaanMaertens <daan.maertens@gmail.com>
2023-11-30 15:31:07 +0100

    forgot to remove ConvertOperator from export

--- src/MPSKit.jl
+++ src/MPSKit.jl
@@ -27,7 +27,7 @@ export entanglementplot, transferplot
 
 # hamiltonian things
 export Cache
-export SparseMPO, MPOHamiltonian, DenseMPO, MPOMultiline, LazySum, ConvertOperator
+export SparseMPO, MPOHamiltonian, DenseMPO, MPOMultiline, LazySum
 export ∂C, ∂AC, ∂AC2, environments, expectation_value, effective_excitation_hamiltonian
 export leftenv, rightenv
 
