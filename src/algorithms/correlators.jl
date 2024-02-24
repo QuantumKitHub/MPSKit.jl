6a89d1e7db2e9fe7703486f71c2eeb88f7eac7ff
lkdvos <lukas.devos@ugent.be>
2024-02-24 12:18:40 +0100

    Formatter

--- src/algorithms/correlators.jl
+++ src/algorithms/correlators.jl
@@ -23,7 +23,7 @@ function correlator(state::AbstractMPS, O₁::MPOTensor, O₂::MPOTensor, i::Int
     U = Tensor(ones, S₁)
 
     @plansor Vₗ[-1 -2; -3] := state.AC[i][3 4; -3] * conj(U[1]) * O₁[1 2; 4 -2] *
-                             conj(state.AC[i][3 2; -1])
+                              conj(state.AC[i][3 2; -1])
     ctr = i + 1
 
     for (k, j) in enumerate(js)
@@ -31,7 +31,7 @@ function correlator(state::AbstractMPS, O₁::MPOTensor, O₂::MPOTensor, i::Int
             Vₗ = Vₗ * TransferMatrix(state.AR[ctr:(j - 1)])
         end
         G[k] = @plansor Vₗ[2 3; 5] * state.AR[j][5 6; 7] * O₂[3 4; 6 1] * U[1] *
-                       conj(state.AR[j][2 4; 7])
+                        conj(state.AR[j][2 4; 7])
         ctr = j
     end
     return G
