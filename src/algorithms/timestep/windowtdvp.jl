5e34180250728638a916c199a3d9a3197464b532
DaanMaertens <daan.maertens@gmail.com>
2024-02-27 13:28:19 +0100

    Update windowtdvp.jl

--- src/algorithms/timestep/windowtdvp.jl
+++ src/algorithms/timestep/windowtdvp.jl
@@ -9,9 +9,10 @@
 - `finalize::F`: user-supplied function which is applied after each timestep, with
     signature `finalize(t, Ψ, H, envs) -> Ψ, envs`. Can be used to enlarge the bond dimension of the infinite part.
 """
-@kwdef struct WindowTDVP{A,B,F} <: Algorithm
-    finite_alg::A = TDVP()
-    infinite_alg::B = TDVP()
+@kwdef struct WindowTDVP{A,B,C,F} <: Algorithm
+    left::A = TDVP()
+    middle::B = TDVP()
+    right::C = left
     finalize::F = Defaults._finalize
 end
 
@@ -19,19 +20,19 @@ function timestep!(Ψ::WindowMPS{A,B,VL,VR},H::Union{Window,LazySum{<:Window}},t
     
     #first evolve left state
     if VL === WINDOW_VARIABLE
-        nleft, _ = timestep(Ψ.left, H.left, t, dt, alg.infinite_alg, env.left; leftorthflag=true) #env gets updated in place
+        nleft, _ = timestep(Ψ.left, H.left, t, dt, alg.left, env.left; leftorthflag=true) #env gets updated in place
         Ψ = WindowMPS(nleft,Ψ.middle,Ψ.right)
     end
 
-    Ψ, env = ltr_sweep!(Ψ, H.middle, t, dt / 2, alg.finite_alg, env)
+    Ψ, env = ltr_sweep!(Ψ, H.middle, t, dt / 2, alg.middle, env)
 
     #then evolve right state
     if VR === WINDOW_VARIABLE
-        nright, _ = timestep(Ψ.right, H.right, t + dt, dt, alg.infinite_alg, env.right; leftorthflag=false) # env gets updated in place
+        nright, _ = timestep(Ψ.right, H.right, t + dt, dt, alg.right, env.right; leftorthflag=false) # env gets updated in place
         Ψ = WindowMPS(Ψ.left,Ψ.middle,nright)
     end
 
-    Ψ, env = rtl_sweep!(Ψ, H.middle, t + dt / 2, dt / 2, alg.finite_alg, env)
+    Ψ, env = rtl_sweep!(Ψ, H.middle, t + dt / 2, dt / 2, alg.middle, env)
     
     return Ψ, env
 end
