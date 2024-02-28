76a77558ca4e9eca06537ec2476caa153a57b6de
DaanMaertens <daan.maertens@gmail.com>
2024-02-28 16:06:22 +0100

    Update timestep.jl

--- src/algorithms/timestep/timestep.jl
+++ src/algorithms/timestep/timestep.jl
@@ -1,6 +1,6 @@
 # TDVP
 
-function timestep!(Ψ::FiniteMPS, H, t::Number, dt::Number, alg::Union{TDVP,TDVP2},
+function timestep!(Ψ::AbstractFiniteMPS, H, t::Number, dt::Number, alg::Union{TDVP,TDVP2},
                    envs::Union{Cache,MultipleEnvironments}=environments(Ψ, H))
     Ψ, envs = ltr_sweep!(Ψ, H, t, dt / 2, alg, envs)
     Ψ, envs = rtl_sweep!(Ψ, H, t + dt / 2, dt / 2, alg, envs)
@@ -9,7 +9,7 @@ function timestep!(Ψ::FiniteMPS, H, t::Number, dt::Number, alg::Union{TDVP,TDVP
 end
 
 #copying version
-function timestep(Ψ::FiniteMPS, H, time::Number, timestep::Number,
+function timestep(Ψ::AbstractFiniteMPS, H, time::Number, timestep::Number,
                   alg::Union{TDVP,TDVP2}, envs=environments(Ψ, H); kwargs...)
     return timestep!(copy(Ψ), H, time, timestep, alg, envs; kwargs...)
 end
