9de95fb20152942e1523959330fed9d152d1170b
lkdvos <lukas.devos@ugent.be>
2024-03-05 23:19:49 +0100

    DMRG logging

--- src/algorithms/groundstate/dmrg.jl
+++ src/algorithms/groundstate/dmrg.jl
@@ -20,32 +20,37 @@ Single site DMRG algorithm for finding groundstates.
 end
 
 function find_groundstate!(ψ::AbstractFiniteMPS, H, alg::DMRG, envs=environments(ψ, H))
-    t₀ = Base.time_ns()
-    ϵ::Float64 = 2 * alg.tol
+    ϵs = map(pos -> calc_galerkin(ψ, pos, envs), 1:length(ψ))
+    ϵ = maximum(ϵs)
+    log = IterLog("DMRG")
 
-    for iter in 1:(alg.maxiter)
-        global ϵ = 0.0
-        Δt = @elapsed begin
+    LoggingExtras.withlevel(; alg.verbosity) do
+        @infov 2 loginit!(log, ϵ, _objective(ψ, H, envs))
+        for iter in 1:(alg.maxiter)
+            alg_eigsolve = updatetol(alg.eigalg, iter, ϵ)
+
+            zerovector!(ϵs)
             for pos in [1:(length(ψ) - 1); length(ψ):-1:2]
                 h = ∂∂AC(pos, ψ, H, envs)
-                _, vecs = eigsolve(h, ψ.AC[pos], 1, :SR, alg.eigalg)
-                ϵ = max(ϵ, calc_galerkin(ψ, pos, envs))
+                _, vecs = eigsolve(h, ψ.AC[pos], 1, :SR, alg_eigsolve)
+                ϵs[pos] = max(ϵs[pos], calc_galerkin(ψ, pos, envs))
                 ψ.AC[pos] = vecs[1]
             end
+            ϵ = maximum(ϵs)
 
             ψ, envs = alg.finalize(iter, ψ, H, envs)::Tuple{typeof(ψ),typeof(envs)}
+
+            if ϵ <= alg.tol
+                @infov 2 logfinish!(log, iter, ϵ, _objective(ψ, H, envs))
+                break
+            end
+            if iter == alg.maxiter
+                @warnv 1 logcancel!(log, iter, ϵ, _objective(ψ, H, envs))
+            else
+                @infov 2 logiter!(log, iter, ϵ, _objective(ψ, H, envs))
+            end
         end
-
-        alg.verbosity ≥ VERBOSE_ITER &&
-            @info "DMRG iteration:" iter ϵ λ = sum(expectation_value(ψ, H, envs)) Δt
-
-        ϵ <= alg.tol && break
-        iter == alg.maxiter &&
-            @warn "DMRG maximum iterations" iter ϵ λ = sum(expectation_value(ψ, H, envs))
     end
-
-    Δt = (Base.time_ns() - t₀) / 1.0e9
-    alg.verbosity ≥ VERBOSE_CONV && @info "DMRG summary:" ϵ λ = sum(expectation_value(ψ, H, envs)) Δt
     return ψ, envs, ϵ
 end
 
@@ -64,67 +69,72 @@ end
 - `trscheme`: truncation algorithm for [tsvd][TensorKit.tsvd](@ref)
 """
 @kwdef struct DMRG2{A,F} <: Algorithm
-    tol = Defaults.tol
-    maxiter = Defaults.maxiter
+    tol::Float64 = Defaults.tol
+    maxiter::Int = Defaults.maxiter
     eigalg::A = Defaults.eigsolver
-    trscheme = truncerr(1e-6)
-    verbosity = Defaults.verbosity
+    trscheme::TruncationScheme = truncerr(1e-6)
+    verbosity::Int = Defaults.verbosity
     finalize::F = Defaults._finalize
 end
 
 function find_groundstate!(ψ::AbstractFiniteMPS, H, alg::DMRG2, envs=environments(ψ, H))
-    t₀ = Base.time_ns()
-    ϵ::Float64 = 2 * alg.tol
+    ϵs = map(pos -> calc_galerkin(ψ, pos, envs), 1:length(ψ))
+    ϵ = maximum(ϵs)
+    log = IterLog("DMRG2")
 
-    for iter in 1:(alg.maxiter)
-        ϵ = 0.0
-        Δt = @elapsed begin
-            #left to right sweep
+    LoggingExtras.withlevel(; alg.verbosity) do
+        for iter in 1:(alg.maxiter)
+            alg_eigsolve = updatetol(alg.eigalg, iter, ϵ)
+            zerovector!(ϵs)
+            
+            # left to right sweep
             for pos in 1:(length(ψ) - 1)
                 @plansor ac2[-1 -2; -3 -4] := ψ.AC[pos][-1 -2; 1] * ψ.AR[pos + 1][1 -4; -3]
 
-                _, vecs = eigsolve(∂∂AC2(pos, ψ, H, envs), ac2, 1, :SR, alg.eigalg)
+                _, vecs = eigsolve(∂∂AC2(pos, ψ, H, envs), ac2, 1, :SR, alg_eigsolve)
                 newA2center = first(vecs)
 
                 al, c, ar, = tsvd!(newA2center; trunc=alg.trscheme, alg=TensorKit.SVD())
                 normalize!(c)
                 v = @plansor ac2[1 2; 3 4] * conj(al[1 2; 5]) * conj(c[5; 6]) *
                              conj(ar[6; 3 4])
-                ϵ = max(ϵ, abs(1 - abs(v)))
+                ϵ[pos] = max(ϵ[pos], abs(1 - abs(v)))
 
                 ψ.AC[pos] = (al, complex(c))
                 ψ.AC[pos + 1] = (complex(c), _transpose_front(ar))
             end
 
+            # right to left sweep
             for pos in (length(ψ) - 2):-1:1
                 @plansor ac2[-1 -2; -3 -4] := ψ.AL[pos][-1 -2; 1] * ψ.AC[pos + 1][1 -4; -3]
 
-                _, vecs = eigsolve(∂∂AC2(pos, ψ, H, envs), ac2, 1, :SR, alg.eigalg)
+                _, vecs = eigsolve(∂∂AC2(pos, ψ, H, envs), ac2, 1, :SR, alg_eigsolve)
                 newA2center = first(vecs)
 
                 al, c, ar, = tsvd!(newA2center; trunc=alg.trscheme, alg=TensorKit.SVD())
                 normalize!(c)
                 v = @plansor ac2[1 2; 3 4] * conj(al[1 2; 5]) * conj(c[5; 6]) *
                              conj(ar[6; 3 4])
-                ϵ = max(ϵ, abs(1 - abs(v)))
+                ϵs[pos] = max(ϵs[pos], abs(1 - abs(v)))
 
                 ψ.AC[pos + 1] = (complex(c), _transpose_front(ar))
                 ψ.AC[pos] = (al, complex(c))
             end
 
+            ϵ = maximum(ϵs)
             ψ, envs = alg.finalize(iter, ψ, H, envs)::Tuple{typeof(ψ),typeof(envs)}
+
+            if ϵ <= alg.tol
+                @infov 2 logfinish!(log, iter, ϵ, _objective(ψ, H, envs))
+                break
+            end
+            if iter == alg.maxiter
+                @warnv 1 logcancel!(log, iter, ϵ, _objective(ψ, H, envs))
+            else
+                @infov 2 logiter!(log, iter, ϵ, _objective(ψ, H, envs))
+            end
         end
-
-        alg.verbosity ≥ VERBOSE_ITER &&
-            @info "DMRG2 iteration:" iter ϵ λ = sum(expectation_value(ψ, H, envs)) Δt
-
-        ϵ <= alg.tol && break
-        iter == alg.maxiter &&
-            @warn "DMRG2 maximum iterations" iter ϵ λ = sum(expectation_value(ψ, H, envs))
     end
-
-    Δt = (Base.time_ns() - t₀) / 1.0e9
-    alg.verbosity ≥ VERBOSE_CONV && @info "DMRG2 summary:" ϵ λ = sum(expectation_value(ψ, H, envs)) Δt
     return ψ, envs, ϵ
 end
 
