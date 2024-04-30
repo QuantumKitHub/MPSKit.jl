3f41f07a2c367fcdd76de453dc4a8286418a2516
lkdvos <lukas.devos@ugent.be>
2024-04-30 10:26:28 +0200

    Add addition for FiniteMPO

--- src/operators/densempo.jl
+++ src/operators/densempo.jl
@@ -7,7 +7,7 @@ Matrix Product Operator (MPO) acting on a finite tensor product space with a lin
 struct FiniteMPO{O<:MPOTensor}
     opp::Vector{O}
     function FiniteMPO(Os::Vector{O}) where {O<:MPOTensor}
-        for i in eachindex(Os)[1:end-1]
+        for i in eachindex(Os)[1:(end - 1)]
             dual(right_virtualspace(Os[i])) == left_virtualspace(Os[i + 1]) ||
                 throw(SpaceMismatch("umatching virtual spaces at site $i"))
         end
@@ -17,7 +17,9 @@ struct FiniteMPO{O<:MPOTensor}
         return new{O}(Os)
     end
 end
-FiniteMPO(O::AbstractTensorMap{S,N,N}) where {S,N} = FiniteMPO(decompose_localmpo(add_util_leg(O)))
+function FiniteMPO(O::AbstractTensorMap{S,N,N}) where {S,N}
+    return FiniteMPO(decompose_localmpo(add_util_leg(O)))
+end
 
 # AbstractVector
 # --------------
@@ -32,7 +34,7 @@ function Base.setindex!(t::FiniteMPO{O}, v::O, i::Int) where {O}
     @boundscheck begin
         checkbounds(t.opp, i)
         left_virtualspace(v) == left_virtualspace(t, i) &&
-        right_virtualspace(v) == right_virtualspace(t, i) ||
+            right_virtualspace(v) == right_virtualspace(t, i) ||
             throw(SpaceMismatch("umatching virtual spaces at site $i"))
     end
     @inbounds t.opp[i] = v
@@ -43,8 +45,8 @@ end
 # ----------
 function Base.convert(::Type{<:FiniteMPS}, mpo::FiniteMPO)
     return FiniteMPS(map(mpo.opp) do O
-                           @plansor A[-1 -2 -3; -4] := O[-1 -2; 1 2] * τ[1 2; -4 -3]
-                       end)
+                         @plansor A[-1 -2 -3; -4] := O[-1 -2; 1 2] * τ[1 2; -4 -3]
+                     end)
 end
 function Base.convert(::Type{<:FiniteMPO}, mps::FiniteMPS)
     mpo_tensors = map([mps.AC[1]; mps.AR[2:end]]) do A
@@ -55,7 +57,89 @@ end
 
 # Linear Algebra
 # --------------
+VectorInterface.scalartype(::Type{FiniteMPO{O}}) where {O} = scalartype(O)
 
+function Base.:+(mpo1::FiniteMPO{TO}, mpo2::FiniteMPO{TO}) where {TO}
+    (N = length(mpo1)) == length(mpo2) || throw(ArgumentError("dimension mismatch"))
+    @assert left_virtualspace(mpo1, 1) == left_virtualspace(mpo2, 1) &&
+            right_virtualspace(mpo1, N) == right_virtualspace(mpo2, N)
+
+    mpo = similar(mpo1.opp)
+    halfN = N ÷ 2
+    A = storagetype(TO)
+
+    # left half
+    F₁ = isometry(A, (right_virtualspace(mpo1, 1) ⊕ right_virtualspace(mpo2, 1))',
+                  right_virtualspace(mpo1, 1)')
+    F₂ = leftnull(F₁)
+    @assert _lastspace(F₂) == right_virtualspace(mpo2, 1)
+
+    @plansor O[-3 -1 -2; -4] := mpo1[1][-1 -2; -3 1] * conj(F₁[-4; 1]) +
+                                mpo2[1][-1 -2; -3 1] * conj(F₂[-4; 1])
+
+    # making sure that the new operator is "full rank"
+    O, R = leftorth!(O)
+    mpo[1] = transpose(O, (2, 3), (1, 4))
+
+    for i in 2:halfN
+        # incorporate fusers from left side
+        @plansor O₁[-1 -2; -3 -4] := R[-1; 1] * F₁[1; 2] * mpo1[i][2 -2; -3 -4]
+        @plansor O₂[-1 -2; -3 -4] := R[-1; 1] * F₂[1; 2] * mpo2[i][2 -2; -3 -4]
+
+        # incorporate fusers from right side
+        F₁ = isometry(A, (right_virtualspace(mpo1, i) ⊕ right_virtualspace(mpo2, i))',
+                      right_virtualspace(mpo1, i)')
+        F₂ = leftnull(F₁)
+        @assert _lastspace(F₂) == right_virtualspace(mpo2, i)
+        @plansor O[-3 -1 -2; -4] := O₁[-1 -2; -3 1] * conj(F₁[-4; 1]) +
+                                    O₂[-1 -2; -3 1] * conj(F₂[-4; 1])
+
+        # making sure that the new operator is "full rank"
+        O, R = leftorth!(O)
+        mpo[i] = transpose(O, (2, 3), (1, 4))
+    end
+
+    C₁, C₂ = F₁, F₂
+
+    # right half
+    F₁ = isometry(A, left_virtualspace(mpo1, N) ⊕ left_virtualspace(mpo2, N),
+                  left_virtualspace(mpo1, N))
+    F₂ = leftnull(F₁)
+    @assert _lastspace(F₂) == left_virtualspace(mpo2, N)'
+
+    @plansor O[-1; -3 -4 -2] := F₁[-1; 1] * mpo1[N][1 -2; -3 -4] +
+                                F₂[-1; 1] * mpo2[N][1 -2; -3 -4]
+
+    # making sure that the new operator is "full rank"
+    L, O = rightorth!(O)
+    mpo[end] = transpose(O, (1, 4), (2, 3))
+
+    for i in (N - 1):-1:(halfN + 1)
+        # incorporate fusers from right side
+        @plansor O₁[-1 -2; -3 -4] := mpo1[i][-1 -2; -3 2] * conj(F₁[1; 2]) * L[1; -4]
+        @plansor O₂[-1 -2; -3 -4] := mpo2[i][-1 -2; -3 2] * conj(F₂[1; 2]) * L[1; -4]
+
+        # incorporate fusers from left side
+        F₁ = isometry(A, left_virtualspace(mpo1, i) ⊕ left_virtualspace(mpo2, i),
+                      left_virtualspace(mpo1, i))
+        F₂ = leftnull(F₁)
+        @assert _lastspace(F₂) == left_virtualspace(mpo2, i)'
+        @plansor O[-1; -3 -4 -2] := F₁[-1; 1] * O₁[1 -2; -3 -4] +
+                                    F₂[-1; 1] * O₂[1 -2; -3 -4]
+
+        # making sure that the new operator is "full rank"
+        L, O = rightorth!(O)
+        mpo[i] = transpose(O, (1, 4), (2, 3))
+    end
+
+    # create center gauge and absorb to the right
+    C₁ = C₁ * F₁'
+    C₂ = C₂ * F₂'
+    C = R * (C₁ + C₂) * L
+    @plansor mpo[halfN + 1][-1 -2; -3 -4] := mpo[halfN + 1][1 -2; -3 -4] * C[-1; 1]
+
+    return FiniteMPO(mpo)
+end
 
 "
     Represents a dense periodic mpo
