261ad0e7c01dc7da4b8647c2c22445450668e4a0
Lukas Devos <ldevos98@gmail.com>
2025-02-28 17:33:52 -0500

    WII for `FiniteMPOHamiltonian`

--- src/algorithms/timestep/wii.jl
+++ src/algorithms/timestep/wii.jl
@@ -19,62 +19,61 @@ $(TYPEDFIELDS)
     maxiter::Int = Defaults.maxiter
 end
 
-function make_time_mpo(H::InfiniteMPOHamiltonian{T}, dt, alg::WII) where {T}
+function make_time_mpo(H::InfiniteMPOHamiltonian, dt::Number, alg::WII)
     WA = H.A
     WB = H.B
     WC = H.C
     WD = H.D
 
     δ = dt * (-1im)
+    exp_alg = Arnoldi(; alg.tol, alg.maxiter)
+
     Wnew = map(1:length(H)) do i
         for j in 2:(size(H[i], 1) - 1), k in 2:(size(H[i], 4) - 1)
-            init_1 = isometry(storagetype(WD[i]), codomain(WD[i]), domain(WD[i]))
-            init = [init_1,
+            init = (isometry(storagetype(WD[i]), codomain(WD[i]), domain(WD[i])),
                     zero(H[i][1, 1, 1, k]),
                     zero(H[i][j, 1, 1, end]),
-                    zero(H[i][j, 1, 1, k])]
+                    zero(H[i][j, 1, 1, k]))
 
-            y, convhist = exponentiate(1.0, init,
-                                       Arnoldi(; tol=alg.tol, maxiter=alg.maxiter)) do x
-                out = similar(x)
+            y, convhist = exponentiate(1.0, init, exp_alg) do (x1, x2, x3, x4)
+                @plansor y1[-1 -2; -3 -4] := δ * x1[-1 1; -3 -4] *
+                                             H[i][1, 1, 1, end][2 3; 1 4] *
+                                             τ[-2 4; 2 3]
 
-                @plansor out[1][-1 -2; -3 -4] := δ * x[1][-1 1; -3 -4] *
-                                                 H[i][1, 1, 1, end][2 3; 1 4] *
-                                                 τ[-2 4; 2 3]
+                @plansor y2[-1 -2; -3 -4] := δ * x2[-1 1; -3 -4] *
+                                             H[i][1, 1, 1, end][2 3; 1 4] *
+                                             τ[-2 4; 2 3] +
+                                             sqrt(δ) *
+                                             x1[1 2; -3 4] *
+                                             H[i][1, 1, 1, k][-1 -2; 3 -4] *
+                                             τ[3 4; 1 2]
 
-                @plansor out[2][-1 -2; -3 -4] := δ * x[2][-1 1; -3 -4] *
-                                                 H[i][1, 1, 1, end][2 3; 1 4] *
-                                                 τ[-2 4; 2 3]
-                @plansor out[2][-1 -2; -3 -4] += sqrt(δ) *
-                                                 x[1][1 2; -3 4] *
-                                                 H[i][1, 1, 1, k][-1 -2; 3 -4] *
-                                                 τ[3 4; 1 2]
+                @plansor y3[-1 -2; -3 -4] := δ * x3[-1 1; -3 -4] *
+                                             H[i][1, 1, 1, end][2 3; 1 4] *
+                                             τ[-2 4; 2 3] +
+                                             sqrt(δ) *
+                                             x1[1 2; -3 4] *
+                                             H[i][j, 1, 1, end][-1 -2; 3 -4] *
+                                             τ[3 4; 1 2]
 
-                @plansor out[3][-1 -2; -3 -4] := δ * x[3][-1 1; -3 -4] *
-                                                 H[i][1, 1, 1, end][2 3; 1 4] *
-                                                 τ[-2 4; 2 3]
-                @plansor out[3][-1 -2; -3 -4] += sqrt(δ) *
-                                                 x[1][1 2; -3 4] *
-                                                 H[i][j, 1, 1, end][-1 -2; 3 -4] *
-                                                 τ[3 4; 1 2]
+                @plansor y4[-1 -2; -3 -4] := (δ * x4[-1 1; -3 -4] *
+                                              H[i][1, 1, 1, end][2 3; 1 4] *
+                                              τ[-2 4; 2 3] +
+                                              x1[1 2; -3 4] *
+                                              H[i][j, 1, 1, k][-1 -2; 3 -4] *
+                                              τ[3 4; 1 2]) +
+                                             (sqrt(δ) *
+                                              x2[1 2; -3 -4] *
+                                              H[i][j, 1, 1, end][-1 -2; 3 4] *
+                                              τ[3 4; 1 2]) +
+                                             (sqrt(δ) *
+                                              x3[-1 4; -3 3] *
+                                              H[i][1, 1, 1, k][2 -2; 1 -4] *
+                                              τ[3 4; 1 2])
 
-                @plansor out[4][-1 -2; -3 -4] := δ * x[4][-1 1; -3 -4] *
-                                                 H[i][1, 1, 1, end][2 3; 1 4] *
-                                                 τ[-2 4; 2 3]
-                @plansor out[4][-1 -2; -3 -4] += x[1][1 2; -3 4] *
-                                                 H[i][j, 1, 1, k][-1 -2; 3 -4] *
-                                                 τ[3 4; 1 2]
-                @plansor out[4][-1 -2; -3 -4] += sqrt(δ) *
-                                                 x[2][1 2; -3 -4] *
-                                                 H[i][j, 1, 1, end][-1 -2; 3 4] *
-                                                 τ[3 4; 1 2]
-                @plansor out[4][-1 -2; -3 -4] += sqrt(δ) *
-                                                 x[3][-1 4; -3 3] *
-                                                 H[i][1, 1, 1, k][2 -2; 1 -4] *
-                                                 τ[3 4; 1 2]
-
-                return out
+                return y1, y2, y3, y4
             end
+
             convhist.converged == 0 &&
                 @warn "failed to exponentiate $(convhist.normres)"
 
@@ -98,4 +97,33 @@ function make_time_mpo(H::InfiniteMPOHamiltonian{T}, dt, alg::WII) where {T}
     end
 
     return InfiniteMPO(PeriodicArray(Wnew))
+end
+
+# Hack to treat FiniteMPOhamiltonians as Infinite
+function make_time_mpo(H::FiniteMPOHamiltonian, dt::Number, alg::WII)
+    H′ = copy(parent(H))
+
+    V_left = left_virtualspace(H[1])
+    V_left′ = BlockTensorKit.oplus(V_left, oneunit(V_left), oneunit(V_left))
+    H′[1] = similar(H[1], V_left′ ⊗ space(H[1], 2) ← domain(H[1]))
+    for (I, v) in nonzero_pairs(H[1])
+        H′[1][I] = v
+    end
+
+    V_right = right_virtualspace(H[end])
+    V_right′ = BlockTensorKit.oplus(oneunit(V_right), oneunit(V_right), V_right)
+    H′[end] = similar(H[end], codomain(H[end]) ← space(H[end], 3)' ⊗ V_right′)
+    for (I, v) in nonzero_pairs(H[end])
+        H′[end][I[1], 1, 1, end] = v
+    end
+
+    H′[1][end, 1, 1, end] = H′[1][1, 1, 1, 1]
+    H′[end][1, 1, 1, 1] = H′[end][end, 1, 1, end]
+
+    mpo = make_time_mpo(InfiniteMPOHamiltonian(H′), dt, alg; tol)
+
+    # Impose boundary conditions
+    mpo[1] = mpo[1][1, :, :, :]
+    mpo[end] = mpo[end][:, :, :, 1]
+    return remove_orphans!(mpo; tol)
 end
