d7ec075f337a60c6a6371cddcfade721dfd20c44
leburgel <lander.burgelman@gmail.com>
2025-03-02 12:13:12 +0100

    Expand `exact_diagonalization` and `FiniteMPS` docstrings

--- src/algorithms/ED.jl
+++ src/algorithms/ED.jl
@@ -8,6 +8,27 @@
 Use [`KrylovKit.eigsolve`](@extref) to perform exact diagonalization on a
 `FiniteMPOHamiltonian` to find its eigenvectors as `FiniteMPS` of maximal rank, essentially
 equivalent to dense eigenvectors.
+
+### Arguments
+- `opp::FiniteMPOHamiltonian`: the Hamiltonian to diagonalize.
+
+### Keyword arguments
+- `sector=first(sectors(oneunit(physicalspace(opp, 1))))`: the total charge of the
+  eigenvectors, which is chosen trivial by default.
+- `len::Int=length(opp)`: the length of the system.
+- `num::Int=1`: the number of eigenvectors to find.
+- `which::Symbol=:SR`: the kind eigenvalues to find, see [`KrylovKit.eigsolve`](@extref). 
+- `alg=Defaults.alg_eigsolve(; dynamic_tols=false)`: the diagonalization algorithm to use,
+  see [`KrylovKit.eigsolve`](@extref).
+
+!!! note "Valid `sector` values"
+    The total charge of the eigenvectors is imposed by adding a charged auxiliary space as
+    the leftmost virtualspace of each eigenvector. Specifically, this is achieved by passing
+    `left=Vect[typeof(sector)](sector => 1)` to the [`FiniteMPS`](@ref) constructor. As
+    such, the only valid `sector` values (i.e. a `sector` value for which the corresponding
+    eigenstate has valid fusion channels) are those that occur in the dual of the fusion of
+    all the physical spaces in the system.
+
 """
 function exact_diagonalization(opp::FiniteMPOHamiltonian;
                                sector=first(sectors(oneunit(physicalspace(opp, 1)))),
