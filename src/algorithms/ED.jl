e9fc5818f75f1c7faf793d8c05ade49bd116a93b
leburgel <lander.burgelman@gmail.com>
2025-03-03 16:41:52 +0100

    And some more `opp -> H`

--- src/algorithms/ED.jl
+++ src/algorithms/ED.jl
@@ -1,7 +1,7 @@
 """
-    exact_diagonalization(opp::FiniteMPOHamiltonian;
-                          sector=first(sectors(oneunit(physicalspace(opp, 1)))),
-                          len::Int=length(opp), num::Int=1, which::Symbol=:SR,
+    exact_diagonalization(H::FiniteMPOHamiltonian;
+                          sector=first(sectors(oneunit(physicalspace(H, 1)))),
+                          len::Int=length(H), num::Int=1, which::Symbol=:SR,
                           alg=Defaults.alg_eigsolve(; dynamic_tols=false))
                             -> vals, state_vecs, convhist
 
@@ -13,9 +13,9 @@ equivalent to dense eigenvectors.
 - `H::FiniteMPOHamiltonian`: the Hamiltonian to diagonalize.
 
 ### Keyword arguments
-- `sector=first(sectors(oneunit(physicalspace(opp, 1))))`: the total charge of the
+- `sector=first(sectors(oneunit(physicalspace(H, 1))))`: the total charge of the
   eigenvectors, which is chosen trivial by default.
-- `len::Int=length(opp)`: the length of the system.
+- `len::Int=length(H)`: the length of the system.
 - `num::Int=1`: the number of eigenvectors to find.
 - `which::Symbol=:SR`: the kind eigenvalues to find, see [`KrylovKit.eigsolve`](@extref). 
 - `alg=Defaults.alg_eigsolve(; dynamic_tols=false)`: the diagonalization algorithm to use,
@@ -30,16 +30,16 @@ equivalent to dense eigenvectors.
     of all the physical spaces in the system.
 
 """
-function exact_diagonalization(opp::FiniteMPOHamiltonian;
-                               sector=first(sectors(oneunit(physicalspace(opp, 1)))),
-                               len::Int=length(opp), num::Int=1, which::Symbol=:SR,
+function exact_diagonalization(H::FiniteMPOHamiltonian;
+                               sector=first(sectors(oneunit(physicalspace(H, 1)))),
+                               len::Int=length(H), num::Int=1, which::Symbol=:SR,
                                alg=Defaults.alg_eigsolve(; dynamic_tols=false))
     left = ℂ[typeof(sector)](sector => 1)
     right = oneunit(left)
 
     middle_site = Int(round(len / 2))
 
-    Ot = eltype(opp[1])
+    Ot = eltype(H[1])
 
     mpst_type = tensormaptype(spacetype(Ot), 2, 1, storagetype(Ot))
     mpsb_type = tensormaptype(spacetype(Ot), 1, 1, storagetype(Ot))
@@ -49,27 +49,27 @@ function exact_diagonalization(opp::FiniteMPOHamiltonian;
     ACs = Vector{Union{Missing,mpst_type}}(missing, len)
 
     for i in 1:(middle_site - 1)
-        ALs[i] = isomorphism(storagetype(Ot), left * physicalspace(opp, i),
-                             fuse(left * physicalspace(opp, i)))
+        ALs[i] = isomorphism(storagetype(Ot), left * physicalspace(H, i),
+                             fuse(left * physicalspace(H, i)))
         left = _lastspace(ALs[i])'
     end
     for i in len:-1:(middle_site + 1)
         ARs[i] = _transpose_front(isomorphism(storagetype(Ot),
-                                              fuse(right * physicalspace(opp, i)'),
-                                              right * physicalspace(opp, i)'))
+                                              fuse(right * physicalspace(H, i)'),
+                                              right * physicalspace(H, i)'))
         right = _firstspace(ARs[i])
     end
-    ACs[middle_site] = randomize!(similar(opp[1][1, 1, 1, 1],
-                                          left * physicalspace(opp, middle_site) ← right))
+    ACs[middle_site] = randomize!(similar(H[1][1, 1, 1, 1],
+                                          left * physicalspace(H, middle_site) ← right))
     norm(ACs[middle_site]) == 0 && throw(ArgumentError("invalid sector"))
     normalize!(ACs[middle_site])
 
     #construct the largest possible finite mps of that length
     state = FiniteMPS(ALs, ARs, ACs, Cs)
-    envs = environments(state, opp)
+    envs = environments(state, H)
 
     #optimize the middle site. Because there is no truncation, this single site captures the entire possible hilbert space
-    H_ac = ∂∂AC(middle_site, state, opp, envs) # this linear operator is now the actual full hamiltonian!
+    H_ac = ∂∂AC(middle_site, state, H, envs) # this linear operator is now the actual full hamiltonian!
     (vals, vecs, convhist) = eigsolve(H_ac, state.AC[middle_site], num, which, alg)
 
     state_vecs = map(vecs) do v
