9703d030a6fd61c0cb45d114ede29a91a5f1e6cc
Lukas Devos <ldevos98@gmail.com>
2025-03-17 15:22:49 -0400

    Remove make-file changes

--- examples/make.jl
+++ examples/make.jl
@@ -11,15 +11,6 @@ using MPSKit
 using Literate
 using TOML, SHA
 
-# Multithreading settings
-using MKL
-using LinearAlgebra.BLAS
-BLAS.set_num_threads(4)
-using ThreadPinning
-ThreadPinning.pinthreads(:cores)
-ThreadPinning.MKL.mkl_set_dynamic(0)
-ThreadPinning.threadinfo()
-
 # ---------------------------------------------------------------------------------------- #
 # Caching
 # ---------------------------------------------------------------------------------------- #
