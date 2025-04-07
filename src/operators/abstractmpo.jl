a791799d6cf3003b5e42b4281e4c59197aa7a273
Lukas Devos <ldevos98@gmail.com>
2025-04-07 13:00:49 -0400

    Remove obsolete code

--- src/operators/abstractmpo.jl
+++ src/operators/abstractmpo.jl
@@ -37,11 +37,6 @@ end
 
 # Utility functions
 # -----------------
-# function jordanmpotensortype(::Type{S}, ::Type{T}) where {S<:VectorSpace,T<:Number}
-#     TT = Base.promote_typejoin(tensormaptype(S, 2, 2, T), BraidingTensor{T,S})
-#     return SparseBlockTensorMap{TT}
-# end
-
 remove_orphans!(mpo::AbstractMPO; tol=eps(real(scalartype(mpo)))^(3 / 4)) = mpo
 function remove_orphans!(mpo::SparseMPO; tol=eps(real(scalartype(mpo)))^(3 / 4))
     droptol!.(mpo, tol)
