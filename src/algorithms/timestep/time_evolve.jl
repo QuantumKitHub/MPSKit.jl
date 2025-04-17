2029ee151dc717c54b3f2e66a3235e588763d143
Lukas Devos <ldevos98@gmail.com>
2025-04-17 17:53:17 -0400

    Add support for imaginary time evolution with real tensors

--- src/algorithms/timestep/time_evolve.jl
+++ src/algorithms/timestep/time_evolve.jl
@@ -5,19 +5,28 @@
 Time-evolve the initial state `ψ₀` with Hamiltonian `H` over a given time span by stepping
 through each of the time points obtained by iterating t_span.
 
-# Arguments
+## Arguments
+
 - `ψ₀::AbstractMPS`: initial state
 - `H::AbstractMPO`: operator that generates the time evolution (can be time-dependent).
 - `t_span::AbstractVector{<:Number}`: time points over which the time evolution is stepped
 - `[alg]`: algorithm to use for the time evolution. Defaults to [`TDVP`](@ref).
 - `[envs]`: MPS environment manager
+
+## Keyword Arguments
+
+- `verbosity::Int=0`: verbosity level for logging
+- `imaginary_evolution::Bool=false`: if true, the time evolution is done with an imaginary time step
+    instead, (i.e. ``\\exp(-Hdt)`` instead of ``\\exp(-iHdt)``). This can be useful for using this
+    function to compute the ground state of a Hamiltonian, or to compute finite-temperature
+    properties of a system.
 """
 function time_evolve end, function time_evolve! end
 
 for (timestep, time_evolve) in zip((:timestep, :timestep!), (:time_evolve, :time_evolve!))
     @eval function $time_evolve(ψ, H, t_span::AbstractVector{<:Number}, alg,
                                 envs=environments(ψ, H);
-                                verbosity::Int=0)
+                                verbosity::Int=0, imaginary_evolution::Bool=false)
         log = IterLog("TDVP")
         LoggingExtras.withlevel(; verbosity) do
             @infov 2 loginit!(log, 0, t)
@@ -25,7 +34,7 @@ for (timestep, time_evolve) in zip((:timestep, :timestep!), (:time_evolve, :time
                 t = t_span[iter]
                 dt = t_span[iter + 1] - t
 
-                ψ, envs = $timestep(ψ, H, t, dt, alg, envs)
+                ψ, envs = $timestep(ψ, H, t, dt, alg, envs; imaginary_evolution)
                 ψ, envs = alg.finalize(t, ψ, H, envs)::Tuple{typeof(ψ),typeof(envs)}
 
                 @infov 3 logiter!(log, iter, 0, t)
@@ -43,18 +52,33 @@ end
 Time-step the state `ψ₀` with Hamiltonian `H` over a given time step `dt` at time `t`,
 solving the Schroedinger equation: ``i ∂ψ/∂t = H ψ``.
 
-# Arguments
+## Arguments
+
 - `ψ₀::AbstractMPS`: initial state
 - `H::AbstractMPO`: operator that generates the time evolution (can be time-dependent).
 - `t::Number`: starting time of time-step
 - `dt::Number`: time-step magnitude
 - `[alg]`: algorithm to use for the time evolution. Defaults to [`TDVP`](@ref).
 - `[envs]`: MPS environment manager
+
+## Keyword Arguments
+
+- `imaginary_evolution::Bool=false`: if true, the time evolution is done with an imaginary time step
+    instead, (i.e. ``\\exp(-Hdt)`` instead of ``\\exp(-iHdt)``). This can be useful for using this
+    function to compute the ground state of a Hamiltonian, or to compute finite-temperature
+    properties of a system.
 """
 function timestep end, function timestep! end
 
 @doc """
-    make_time_mpo(H::MPOHamiltonian, dt::Number, alg) -> O::MPO
+    make_time_mpo(H::MPOHamiltonian, dt::Number, alg; kwargs...) -> O::MPO
 
 Construct an `MPO` that approximates ``\\exp(-iHdt)``.
+
+## Keyword Arguments
+
+- `imaginary_evolution::Bool=false`: if true, the time evolution operator is constructed
+    with an imaginary time step instead, (i.e. ``\\exp(-Hdt)`` instead of ``\\exp(-iHdt)``).
+    This can be useful for using this function to compute the ground state of a Hamiltonian,
+    or to compute finite-temperature properties of a system.
 """ make_time_mpo
