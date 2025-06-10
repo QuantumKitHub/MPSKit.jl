f568c9ca656e3f948e851a08d45e075ebb7c7a60
Lukas Devos <ldevos98@gmail.com>
2025-06-09 21:28:50 -0400

    Fix braille checks

--- test/other.jl
+++ test/other.jl
@@ -70,13 +70,19 @@ end
     buffer = IOBuffer()
     braille(buffer, H)
     output = String(take!(buffer))
-    check = "... 🭻⎡⠉⢀⎤🭻 ...\n     ⎣⠀⢀⎦ \n"
+    check = """
+            ... 🭻⎡⢉⢈⎤🭻 ...
+                 ⎣⠀⢀⎦ 
+            """
     @test output == check
 
     O = make_time_mpo(H, 1.0, TaylorCluster(3, false, false))
     braille(buffer, O)
     output = String(take!(buffer))
-    check = "... 🭻⎡⡏⠉⠒⠔⎤🭻 ...\n     ⎣⡇⠀⠀⡂⎦ \n"
+    check = """
+            ... 🭻⎡⡏⠉⠛⠟⎤🭻 ...
+                 ⎣⡇⠀⠀⡂⎦ 
+            """
     @test output == check
 
     # Finite Hamiltonians and MPOs
@@ -84,13 +90,13 @@ end
     H = transverse_field_ising(; L=4)
     braille(buffer, H)
     output = String(take!(buffer))
-    check = " ⎡⠉⠀⎤🭻🭻⎡⠉⢀⎤🭻🭻⎡⠉⢀⎤🭻🭻⎡⡀⠀⎤ \n ⎣⠀⠀⎦  ⎣⠀⢀⎦  ⎣⠀⢀⎦  ⎣⡀⠀⎦ \n"
+    check = " ⎡⠉⠈⎤🭻🭻⎡⢉⢈⎤🭻🭻⎡⢉⢈⎤🭻🭻⎡⡁⠀⎤ \n ⎣⠀⠀⎦  ⎣⠀⢀⎦  ⎣⠀⢀⎦  ⎣⡀⠀⎦ \n"
     @test output == check
 
     O = make_time_mpo(H, 1.0, TaylorCluster(3, false, false))
     braille(buffer, O)
     output = String(take!(buffer))
-    check = " ⎡⠉⠉⎤🭻🭻⎡⠍⠉⠤⠠⎤🭻🭻⎡⡏⠉⠒⠔⎤🭻🭻⎡⡇⠀⎤ \n ⎣⠀⠀⎦  ⎣⡂⠀⠀⠂⎦  ⎣⡇⠀⠀⡂⎦  ⎣⡇⠀⎦ \n"
+    check = " ⎡⠉⠉⠉⠉⎤🭻🭻⎡⡏⠉⠛⠟⎤🭻🭻⎡⡏⠉⠛⠟⎤🭻🭻⎡⡇⠀⎤ \n ⎣⠀⠀⠀⠀⎦  ⎣⡇⠀⠀⡂⎦  ⎣⡇⠀⠀⡂⎦  ⎣⡇⠀⎦ \n"
     @test output == check
 end
 end
