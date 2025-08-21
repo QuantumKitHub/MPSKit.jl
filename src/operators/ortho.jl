84273bf542f5a710923c175d6133c9a0b9aeb251
Lukas Devos <ldevos98@gmail.com>
2025-08-21 11:25:55 +0200

    little bit of cleanup

--- src/operators/ortho.jl
+++ src/operators/ortho.jl
@@ -1,42 +1,8 @@
-function TensorKit.leftorth(W::JordanMPOTensor; alg = QRpos())
-    # orthogonalize second column against first
-    WI = removeunit(W[1, 1, 1, 1], 1)
-    @tensor t[l; r] := conj(WI[p; p' l]) * W.C[p; p' r]
-    I = sectortype(W)
-    S = spacetype(W)
-
-    @plansor t[l; r] := conj(removeunit(W[1, 1, 1, 1], 1)[p; p' l]) * W.C[p; p' r]
-    # @plansor C′[p; p' r] := W.C[p; p' r] - WI[p; p' l] * t[l; r]
-    @plansor C′[p; p' r] := -WI[p; p' l] * t[l; r]
-    add!(C′, W.C)
-
-    # QR of second column
-    CA = transpose(cat(insertleftunit(C′, 1), W.A; dims = 1), ((3, 1, 2), (4,)))
-    Q, r = leftorth!(CA; alg)
-    Q′ = transpose(Q, ((2, 3), (1, 4)))
-    V = codomain(W) ← physicalspace(W) ⊗ BlockTensorKit.oplus(oneunit(S), right_virtualspace(Q′), oneunit(S))
-    Q1 = SparseBlockTensorMap(Q′[2:end, 1, 1, 1])
-    Q2 = removeunit(SparseBlockTensorMap(Q′[1:1, 1, 1, 1]), 1)
-
-    # Assemble output
-    W′ = JordanMPOTensor(V, Q1, W.B, Q2, W.D)
-
-    R = similar(W, right_virtualspace(W′) ← right_virtualspace(W′))
-    R[1, 1] = id!(R[1, 1])
-    R[1, 2] = t
-    R[2, 2] = r
-    R[3, 3] = id!(R[3, 3])
-
-    return W′, R
-end
-
 function left_canonicalize!(
-        H::MPOHamiltonian, i::Int;
+        H::FiniteMPOHamiltonian, i::Int;
         alg = QRpos(), trscheme::TruncationScheme = notrunc()
     )
-    if H isa FiniteMPOHamiltonian
-        1 ≤ i < length(H) || throw(ArgumentError("Bounds error in canonicalize"))
-    end
+    1 ≤ i < length(H) || throw(ArgumentError("Bounds error in canonicalize"))
 
     W = H[i]
     S = spacetype(W)
@@ -105,12 +71,10 @@ function left_canonicalize!(
 end
 
 function right_canonicalize!(
-        H::MPOHamiltonian, i::Int;
+        H::FiniteMPOHamiltonian, i::Int;
         alg = LQpos(), trscheme::TruncationScheme = notrunc()
     )
-    if H isa FiniteMPOHamiltonian
-        1 < i ≤ length(H) || throw(ArgumentError("Bounds error in canonicalize"))
-    end
+    1 < i ≤ length(H) || throw(ArgumentError("Bounds error in canonicalize"))
 
     W = H[i]
     S = spacetype(W)
