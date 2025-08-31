f4de7362d737e433e762431a234f6f69fd7c30a9
Lukas Devos <ldevos98@gmail.com>
2025-08-31 13:04:31 +0200

    Merge transverse field ising implementations

--- test/setup.jl
+++ test/setup.jl
@@ -142,25 +142,9 @@ function S_zz(::Type{Z2Irrep}, ::Type{T} = ComplexF64; spin = 1 // 2) where {T <
     return ZZ
 end
 
-function transverse_field_ising(::Type{Trivial} = Trivial, ::Type{T} = ComplexF64; g = 1.0, L = Inf) where {T <: Number}
-    X = S_x(Trivial, T; spin = 1 // 2)
-    ZZ = S_zz(Trivial, T; spin = 1 // 2)
-
-    if L == Inf
-        lattice = PeriodicArray([ℂ^2])
-        H₁ = InfiniteMPOHamiltonian(lattice, i => -g * X for i in 1:1)
-        H₂ = InfiniteMPOHamiltonian(lattice, (i, i + 1) => -ZZ for i in 1:1)
-    else
-        lattice = fill(ℂ^2, L)
-        H₁ = FiniteMPOHamiltonian(lattice, i => -g * X for i in 1:L)
-        H₂ = FiniteMPOHamiltonian(lattice, (i, i + 1) => -ZZ for i in 1:(L - 1))
-    end
-    return H₁ + H₂
-end
-
-function transverse_field_ising(::Type{Z2Irrep}, ::Type{T} = ComplexF64; g = 1.0, L = Inf) where {T <: Number}
-    X = S_x(Z2Irrep, T; spin = 1 // 2)
-    ZZ = S_zz(Z2Irrep, T; spin = 1 // 2)
+function transverse_field_ising(sym::Type{<:Sector} = Trivial, ::Type{T} = ComplexF64; g = 1.0, L = Inf) where {T <: Number}
+    X = S_x(sym, T; spin = 1 // 2)
+    ZZ = S_zz(sym, T; spin = 1 // 2)
 
     if L == Inf
         lattice = PeriodicArray([space(X, 1)])
