35ccb4bc4d704f155cf10124aaffb7327bdabef4
Lukas Devos <ldevos98@gmail.com>
2025-09-18 13:36:17 +0200

    fix timedependent integrator

--- src/algorithms/timestep/integrators.jl
+++ src/algorithms/timestep/integrators.jl
@@ -22,7 +22,7 @@ function integrate(
         imaginary_evolution::Bool = false
     )
     dt′ = imaginary_evolution ? -dt : -1im * dt
-    y, convhist = exponentiate(_eval_t(f, t + dt′ / 2), dt′, y₀, alg)
+    y, convhist = exponentiate(_eval_t(f, t + dt / 2), dt′, y₀, alg)
     convhist.converged == 0 &&
         @warn "integrator failed to converge: normres = $(convhist.normres)"
     return y
