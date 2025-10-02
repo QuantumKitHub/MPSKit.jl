33db24f8f66cd3cb5ae88b2e1d9bc6b5d403de3e
Yue Zhengyuan <yuezy1997@icloud.com>
2025-10-02 20:31:34 +0800

    Fix again

--- src/utility/utility.jl
+++ src/utility/utility.jl
@@ -112,7 +112,7 @@ function randomize!(a::AbstractBlockTensorMap)
     return a
 end
 
-_totuple(t) = t isa Tuple ? t : tuple(t)
+_totuple(t) = t isa Tuple ? t : (t isa Symbol ? tuple(t) : Tuple(t))
 
 """
     tensorexpr(name, ind_out, [ind_in])
