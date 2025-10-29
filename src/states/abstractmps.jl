0f0037c98a1596f3b39a85c46942e83bda7779df
Lukas Devos <ldevos98@gmail.com>
2025-10-29 19:43:25 -0400

    reorganization

--- src/states/abstractmps.jl
+++ src/states/abstractmps.jl
@@ -85,9 +85,108 @@ function MPSTensor(A::AbstractArray{T}) where {T <: Number}
     t[] .= A
     return t
 end
+# Tensor accessors
+# ----------------
+@doc """
+    AC2(ψ::AbstractMPS, i; kind=:ACAR)
 
-# MPS characterization
-# --------------------
+Obtain the two-site (center) gauge tensor at site `i` of the MPS `ψ`.
+If this hasn't been computed before, this can be computed as:
+- `kind=:ACAR` : AC[i] * AR[i+1]
+- `kind=:ALAC` : AL[i] * AC[i+1]
+""" AC2
+
+# ===========================================================================================
+# MPS types
+# ===========================================================================================
+
+abstract type AbstractMPS end
+abstract type AbstractFiniteMPS <: AbstractMPS end
+
+Base.eltype(ψ::AbstractMPS) = eltype(typeof(ψ))
+VectorInterface.scalartype(T::Type{<:AbstractMPS}) = scalartype(site_type(T))
+
+function Base.checkbounds(ψ::AbstractMPS, i)
+    return Base.checkbounds(Bool, ψ, i) || throw(BoundsError(ψ, i))
+end
+
+"""
+    site_type(ψ::AbstractMPS)
+    site_type(ψtype::Type{<:AbstractMPS})
+
+Return the type of the site tensors of an `AbstractMPS`.
+"""
+site_type(ψ::AbstractMPS) = site_type(typeof(ψ))
+
+"""
+    bond_type(ψ::AbstractMPS)
+    bond_type(ψtype::Type{<:AbstractMPS})
+
+Return the type of the bond tensors of an `AbstractMPS`.
+"""
+bond_type(ψ::AbstractMPS) = bond_type(typeof(ψ))
+
+TensorKit.spacetype(ψ::AbstractMPS) = spacetype(typeof(ψ))
+TensorKit.spacetype(ψtype::Type{<:AbstractMPS}) = spacetype(site_type(ψtype))
+TensorKit.sectortype(ψ::AbstractMPS) = sectortype(typeof(ψ))
+TensorKit.sectortype(ψtype::Type{<:AbstractMPS}) = sectortype(site_type(ψtype))
+
+"""
+    left_virtualspace(ψ::AbstractMPS, [pos=1:length(ψ)])
+
+Return the virtual space of the bond to the left of sites `pos`.
+
+!!! warning
+    In rare cases, the gauge tensor on the virtual space might not be square, and as a result it
+    cannot always be guaranteed that `right_virtualspace(ψ, i - 1) == left_virtualspace(ψ, i)`
+"""
+function left_virtualspace end
+left_virtualspace(A::GenericMPSTensor) = space(A, 1)
+left_virtualspace(O::MPOTensor) = space(O, 1)
+left_virtualspace(ψ::AbstractMPS) = map(Base.Fix1(left_virtualspace, ψ), eachsite(ψ))
+
+"""
+    right_virtualspace(ψ::AbstractMPS, [pos=1:length(ψ)])
+
+Return the virtual space of the bond to the right of site(s) `pos`.
+
+!!! warning
+    In rare cases, the gauge tensor on the virtual space might not be square, and as a result it
+    cannot always be guaranteed that `right_virtualspace(ψ, i - 1) == left_virtualspace(ψ, i)`
+"""
+function right_virtualspace end
+right_virtualspace(A::GenericMPSTensor) = space(A, numind(A))'
+right_virtualspace(O::MPOTensor) = space(O, 4)'
+right_virtualspace(ψ::AbstractMPS) = map(Base.Fix1(right_virtualspace, ψ), eachsite(ψ))
+
+"""
+    physicalspace(ψ::AbstractMPS, [pos=1:length(ψ)])
+
+Return the physical space of the site tensor at site `i`.
+"""
+function physicalspace end
+physicalspace(A::MPSTensor) = space(A, 2)
+physicalspace(A::GenericMPSTensor) = prod(x -> space(A, x), 2:(numind(A) - 1))
+physicalspace(O::MPOTensor) = space(O, 2)
+physicalspace(O::AbstractBlockTensorMap{<:Any, <:Any, 2, 2}) = only(space(O, 2))
+physicalspace(ψ::AbstractMPS) = map(Base.Fix1(physicalspace, ψ), eachsite(ψ))
+
+"""
+    eachsite(state::AbstractMPS)
+
+Return an iterator over the sites of the MPS `state`.
+"""
+eachsite(ψ::AbstractMPS) = eachindex(ψ)
+
+# ===========================================================================================
+# MPS manifolds
+# ===========================================================================================
+"""
+    abstract type AbstractMPSManifold{S <: ElementarySpace}
+
+Abstract supertype for the characterization of an MPS manifold, i.e. the sizes of all tensors that are involved.
+These types are used mostly as convenient dispatch 
+"""
 abstract type AbstractMPSManifold{S <: ElementarySpace} end
 
 TensorKit.spacetype(::Type{<:AbstractMPSManifold{S}}) where {S} = S
@@ -99,7 +198,29 @@ right_virtualspace(manifold::AbstractMPSManifold, i::Integer) = right_virtualspa
 Base.getindex(manifold::AbstractMPSManifold, site::Integer) =
     left_virtualspace(manifold, site) ⊗ physicalspace(manifold, site) ← right_virtualspace(manifold, site)
 
-struct FiniteMPSManifold{S <: ElementarySpace, S′ <: Union{S, CompositeSpace{S}}} <: AbstractMPSManifold{S}
+"""
+    FiniteMPSManifold(pspaces, vspaces) <: AbstractMPSManifold{S}
+
+Full characterization of all [`FiniteMPS`](@ref) spaces. Both `pspaces` and `vspaces` are `Vector`s that hold the
+local physical and virtual spaces, such that we have `length(pspaces) + 1 == length(vspaces)`.
+These objects can be used to construct MPS for a given space, for example through [`Base.randn`](@ref) or similar functions.
+
+## Constructors
+
+    FiniteMPSManifold(physicalspaces::AbstractVector{<:TensorSpace}, max_virtualspaces; [left_virtualspace], [right_virtualspace])
+
+To construct a `FiniteMPSManifold`, you should provide the `physicalspaces`, along with the maximal desired virtual space.
+This latter can be provided as a single `<:ElementarySpace`, or, if a site-dependent maximum is desired through a vector thereof.
+In that case, `length(physicalspaces) == length(max_virtualspaces) + 1`.
+
+It is also possible to add non-trivial virtual spaces on both the left and the right edge of the MPS.
+This might be useful to construct "charged" MPS, or to work with [`WindowMPS`](@ref) that may be embedded in a larger system.
+
+!!! note
+    As the supplied virtual spaces are interpreted as a maximum, this function will automatically construct "full-rank" spaces.
+    These are chosen such that every MPS tensor that would be generated could have full rank. See also [`makefullrank!](@ref).
+"""
+struct FiniteMPSManifold{S <: ElementarySpace, S′ <: TensorSpace{S}} <: AbstractMPSManifold{S}
     pspaces::Vector{S′}
     vspaces::Vector{S}
 end
@@ -107,7 +228,7 @@ end
 function FiniteMPSManifold(
         physicalspaces::AbstractVector{S′}, max_virtualspaces::AbstractVector{S};
         left_virtualspace::S = oneunit(S), right_virtualspace::S = oneunit(S)
-    ) where {S <: ElementarySpace, S′ <: Union{S, CompositeSpace{S}}}
+    ) where {S <: ElementarySpace, S′ <: TensorSpace{S}}
     L₁ = length(physicalspaces)
     L₂ = length(max_virtualspaces)
     L₁ == L₂ + 1 ||
@@ -123,10 +244,29 @@ function FiniteMPSManifold(
 end
 function FiniteMPSManifold(
         physicalspaces::AbstractVector{S′}, max_virtualspace::S; kwargs...
-    ) where {S <: ElementarySpace, S′ <: Union{S, CompositeSpace{S}}}
+    ) where {S <: ElementarySpace, S′ <: TensorSpace{S}}
     return FiniteMPSManifold(physicalspaces, fill(max_virtualspace, length(physicalspaces) - 1))
 end
 
+"""
+    InfiniteMPSManifold(pspaces, vspaces) <: AbstractMPSManifold{S}
+
+Full characterization of all [`InfiniteMPS`](@ref) spaces. Both `pspaces` and `vspaces` are `PeriodicVector`s that hold the
+local physical and virtual spaces, such that we must have `length(pspaces) == length(vspaces)`.
+These objects can be used to construct MPS for a given space, for example through [`Base.randn`](@ref) or similar functions.
+
+## Constructors
+
+    FiniteMPSManifold(physicalspaces::AbstractVector{<:TensorSpace}, max_virtualspaces; [left_virtualspace], [right_virtualspace])
+
+To construct a `FiniteMPSManifold`, you should provide the `physicalspaces`, along with the maximal desired virtual space.
+This latter can be provided as a single `<:ElementarySpace`, or, if a site-dependent maximum is desired through a vector thereof.
+In that case, `length(physicalspaces) == length(max_virtualspaces)`.
+
+!!! note
+    As the supplied virtual spaces are interpreted as a maximum, this function will automatically construct "full-rank" spaces.
+    These are chosen such that every MPS tensor that would be generated could have full rank. See also [`makefullrank!](@ref).
+"""
 struct InfiniteMPSManifold{S <: ElementarySpace, S′ <: Union{S, CompositeSpace{S}}} <: AbstractMPSManifold{S}
     pspaces::PeriodicVector{S′}
     vspaces::PeriodicVector{S}
@@ -156,6 +296,50 @@ left_virtualspace(manifold::FiniteMPSManifold) = manifold.vspaces[1:(end - 1)]
 left_virtualspace(manifold::InfiniteMPSManifold) = manifold.vspaces
 right_virtualspace(manifold::FiniteMPSManifold) = manifold.vspaces[2:end]
 right_virtualspace(manifold::InfiniteMPSManifold) = PeriodicVector(circshift(manifold.vspaces, 1))
+
+# MPS constructors
+# ----------------
+for randf in (:rand, :randn, :randexp, :randisometry)
+    _docstr = """
+        $randf([rng=default_rng()], [T=Float64], manifold::AbstractMPSManifold) -> mps
+        
+    Generate an `mps` with tensors generated by `$randf`.
+
+    See also [`Random.$(randf)!`](@ref).
+    """
+    _docstr! = """
+        $(randf)!([rng=default_rng()], mps::AbstractMPS) -> mps
+        
+    Fill the tensors of `mps` with entries generated by `$(randf)!`.
+
+    See also [`Random.$(randf)`](@ref).
+    """
+
+    if randf != :randisometry
+        randfun = GlobalRef(Random, randf)
+        randfun! = GlobalRef(Random, Symbol(randf, :!))
+    else
+        randfun = randf
+        randfun! = Symbol(randf, :!)
+    end
+
+    @eval begin
+        @doc $_docstr $randfun(::Type, ::AbstractMPSManifold)
+        @doc $_docstr! $randfun!(::AbstractMPSManifold)
+
+        # filling in default eltype
+        $randfun(manifold::AbstractMPSManifold) = $randfun(Defaults.eltype, manifold)
+        function $randfun(rng::Random.AbstractRNG, manifold::AbstractMPSManifold)
+            return $randfun(rng, Defaults.eltype, manifold)
+        end
+
+        # filling in default rng
+        function $randfun(::Type{T}, manifold::AbstractMPSManifold) where {T}
+            return $randfun(Random.default_rng(), T, manifold)
+        end
+        $randfun!(mps::AbstractMPS) = $randfun!(Random.default_rng(), mps)
+    end
+end
 
 """
     isfullrank(A::GenericMPSTensor; side=:both)
@@ -242,97 +426,3 @@ function makefullrank!(A::PeriodicVector{<:GenericMPSTensor}; alg_leftorth = Def
     end
     return A
 end
-
-# Tensor accessors
-# ----------------
-@doc """
-    AC2(ψ::AbstractMPS, i; kind=:ACAR)
-
-Obtain the two-site (center) gauge tensor at site `i` of the MPS `ψ`.
-If this hasn't been computed before, this can be computed as:
-- `kind=:ACAR` : AC[i] * AR[i+1]
-- `kind=:ALAC` : AL[i] * AC[i+1]
-""" AC2
-
-#===========================================================================================
-MPS types
-===========================================================================================#
-
-abstract type AbstractMPS end
-abstract type AbstractFiniteMPS <: AbstractMPS end
-
-Base.eltype(ψ::AbstractMPS) = eltype(typeof(ψ))
-VectorInterface.scalartype(T::Type{<:AbstractMPS}) = scalartype(site_type(T))
-Base.isfinite(ψ::AbstractMPS) = isfinite(typeof(ψ))
-
-function Base.checkbounds(ψ::AbstractMPS, i)
-    return Base.checkbounds(Bool, ψ, i) || throw(BoundsError(ψ, i))
-end
-
-"""
-    site_type(ψ::AbstractMPS)
-    site_type(ψtype::Type{<:AbstractMPS})
-
-Return the type of the site tensors of an `AbstractMPS`.
-"""
-site_type(ψ::AbstractMPS) = site_type(typeof(ψ))
-
-"""
-    bond_type(ψ::AbstractMPS)
-    bond_type(ψtype::Type{<:AbstractMPS})
-
-Return the type of the bond tensors of an `AbstractMPS`.
-"""
-bond_type(ψ::AbstractMPS) = bond_type(typeof(ψ))
-
-TensorKit.spacetype(ψ::AbstractMPS) = spacetype(typeof(ψ))
-TensorKit.spacetype(ψtype::Type{<:AbstractMPS}) = spacetype(site_type(ψtype))
-TensorKit.sectortype(ψ::AbstractMPS) = sectortype(typeof(ψ))
-TensorKit.sectortype(ψtype::Type{<:AbstractMPS}) = sectortype(site_type(ψtype))
-
-"""
-    left_virtualspace(ψ::AbstractMPS, [pos=1:length(ψ)])
-    
-Return the virtual space of the bond to the left of sites `pos`.
-
-!!! warning
-    In rare cases, the gauge tensor on the virtual space might not be square, and as a result it
-    cannot always be guaranteed that `right_virtualspace(ψ, i - 1) == left_virtualspace(ψ, i)`
-"""
-function left_virtualspace end
-left_virtualspace(A::GenericMPSTensor) = space(A, 1)
-left_virtualspace(O::MPOTensor) = space(O, 1)
-left_virtualspace(ψ::AbstractMPS) = map(Base.Fix1(left_virtualspace, ψ), eachsite(ψ))
-
-"""
-    right_virtualspace(ψ::AbstractMPS, [pos=1:length(ψ)])
-
-Return the virtual space of the bond to the right of site(s) `pos`.
-
-!!! warning
-    In rare cases, the gauge tensor on the virtual space might not be square, and as a result it
-    cannot always be guaranteed that `right_virtualspace(ψ, i - 1) == left_virtualspace(ψ, i)`
-"""
-function right_virtualspace end
-right_virtualspace(A::GenericMPSTensor) = space(A, numind(A))'
-right_virtualspace(O::MPOTensor) = space(O, 4)'
-right_virtualspace(ψ::AbstractMPS) = map(Base.Fix1(right_virtualspace, ψ), eachsite(ψ))
-
-"""
-    physicalspace(ψ::AbstractMPS, [pos=1:length(ψ)])
-
-Return the physical space of the site tensor at site `i`.
-"""
-function physicalspace end
-physicalspace(A::MPSTensor) = space(A, 2)
-physicalspace(A::GenericMPSTensor) = prod(x -> space(A, x), 2:(numind(A) - 1))
-physicalspace(O::MPOTensor) = space(O, 2)
-physicalspace(O::AbstractBlockTensorMap{<:Any, <:Any, 2, 2}) = only(space(O, 2))
-physicalspace(ψ::AbstractMPS) = map(Base.Fix1(physicalspace, ψ), eachsite(ψ))
-
-"""
-    eachsite(state::AbstractMPS)
-
-Return an iterator over the sites of the MPS `state`.
-"""
-eachsite(ψ::AbstractMPS) = eachindex(ψ)
