cd43827c63a9726cccb0c44dfa5f99fe7ed77a98
Akshay Shankar <sakshays.2000@gmail.com>
2025-10-31 15:25:45 +0100

    formatting

--- examples/quantum1d/8.bose-hubbard/main.jl
+++ examples/quantum1d/8.bose-hubbard/main.jl
@@ -3,7 +3,7 @@ using MPSKit, MPSKitModels, TensorKit
 using Plots, LaTeXStrings
 
 theme(:wong)
-default(fontfamily="Computer Modern", label=nothing, dpi=100, framestyle=:box)
+default(fontfamily = "Computer Modern", label = nothing, dpi = 100, framestyle = :box)
 
 md"""
 # 1D Bose-Hubbard model
@@ -62,21 +62,21 @@ md"""
 This simply initializes a tensor filled with random entries (check out the documentation for other useful constructors). Next, we need the creation and annihilation operators. While we could construct them from scratch, here we will use `MPSKitModels.jl` instead that has predefined operators and models for most well-known lattice models.  
 """
 
-a_op = a_min(cutoff=cutoff) # creates a bosonic annihilation operator without any symmetries
+a_op = a_min(cutoff = cutoff) # creates a bosonic annihilation operator without any symmetries
 display(a_op[])
-display((a_op'*a_op)[])
+display((a_op' * a_op)[])
 
 md"""
 The [] accessor lets us see the underlying array and indeed the operators are exactly what we require. Similarly, the Bose Hubbard model is also predefined (although we will construct our own variant later on).
 """
 
-hamiltonian = bose_hubbard_model(InfiniteChain(1), cutoff=cutoff, U=1, mu=0.5, t=0.2) # It is not strictly required to pass InfiniteChain() and is only included for clarity; one may instead pass FiniteChain(N) as well
+hamiltonian = bose_hubbard_model(InfiniteChain(1), cutoff = cutoff, U = 1, mu = 0.5, t = 0.2) # It is not strictly required to pass InfiniteChain() and is only included for clarity; one may instead pass FiniteChain(N) as well
 
 md"""
 This has created the Hamiltonian operator as a matrix product operator (MPO) which is a convenient form to use in conjunction with MPS. Finally, the ground state optimization may be performed with either `iDMRG` or `VUMPS`. Both should take similar arguments but it is known that VUMPS is typically more efficient for these systems so we proceed with that.
 """
 
-ground_state, _, _ = find_groundstate(initial_state, hamiltonian, VUMPS(tol=1e-6, verbosity=2, maxiter=200))
+ground_state, _, _ = find_groundstate(initial_state, hamiltonian, VUMPS(tol = 1.0e-6, verbosity = 2, maxiter = 200))
 println("Energy: ", expectation_value(ground_state, hamiltonian))
 
 md"""
@@ -84,30 +84,30 @@ This automatically runs the algorithm until a certain error [measure](https://gi
 """
 
 function get_ground_state(mu, t, cutoff, D; kwargs...)
-    hamiltonian = bose_hubbard_model(InfiniteChain(), cutoff=cutoff, U=1, mu=mu, t=t)
+    hamiltonian = bose_hubbard_model(InfiniteChain(), cutoff = cutoff, U = 1, mu = mu, t = t)
     state = InfiniteMPS(ℂ^(cutoff + 1), ℂ^D)
     state, _, _ = find_groundstate(state, hamiltonian, VUMPS(; kwargs...))
 
     return state
 end
 
-ground_state = get_ground_state(0.5, 0.01, cutoff, D; tol=1e-6, verbosity=2, maxiter=500)
+ground_state = get_ground_state(0.5, 0.01, cutoff, D; tol = 1.0e-6, verbosity = 2, maxiter = 500)
 
 md"""
 Now that we have the state, we may compute observables using the `expectation_value` function. It typically expects a `Pair`, `(i1, i2, .., ik) => op` where `op` is a `TensorMap` or `InfiniteMPO` acting over `k` sites. In case of the Hamiltonian, it is not necessary to specify the indices as it spans the whole lattice. We can now plot the correlation function $\langle \hat{a}^{\dagger}_i \hat{a}_j\rangle$.
 """
 
-plot(map(i -> real.(expectation_value(ground_state, (0, i) => a_op' ⊗ a_op)), 1:50), lw=2, xlabel="Site index", ylabel="Correlation function", yscale=:log10)
-hline!([abs2(expectation_value(ground_state, (0,) => a_op))], ls=:dash, c=:black)
+plot(map(i -> real.(expectation_value(ground_state, (0, i) => a_op' ⊗ a_op)), 1:50), lw = 2, xlabel = "Site index", ylabel = "Correlation function", yscale = :log10)
+hline!([abs2(expectation_value(ground_state, (0,) => a_op))], ls = :dash, c = :black)
 
 md"""
 We see that the correlations drop off exponentially, indicating the existence of a gapped Mott insulating phase. Let us now shift our parameters to probe other phases.
 """
 
-ground_state = get_ground_state(0.5, 0.2, cutoff, D; tol=1e-6, verbosity=2, maxiter=500)
+ground_state = get_ground_state(0.5, 0.2, cutoff, D; tol = 1.0e-6, verbosity = 2, maxiter = 500)
 
-plot(map(i -> real.(expectation_value(ground_state, (0, i) => a_op' ⊗ a_op)), 1:100), lw=2, xlabel="Site index", ylabel="Correlation function", yscale=:log10, xscale=:log10)
-hline!([abs2(expectation_value(ground_state, (0,) => a_op))], ls=:dash, c=:black)
+plot(map(i -> real.(expectation_value(ground_state, (0, i) => a_op' ⊗ a_op)), 1:100), lw = 2, xlabel = "Site index", ylabel = "Correlation function", yscale = :log10, xscale = :log10)
+hline!([abs2(expectation_value(ground_state, (0,) => a_op))], ls = :dash, c = :black)
 
 md"""
 In this case, the correlation function drops off algebraically and eventually saturates as $\lim_{i \to \infty}\langle\hat{a}_i^{\dagger} \hat{a}_j\rangle ≈ \langle \hat{a}_i^{\dagger}\rangle \langle \hat{a}_j \rangle = |\langle a_i \rangle|^2 \neq 0$. This is a signature of long-range order and suggests the existence of a Bose-Einstein condensate. However, this is a bit odd since at zero temperature, the Bose Hubbard model is not expected to break any continuous symmetries ($U(1)$ in this case, corresponding to particle number conservation) due to the Mermin-Wagner theorem. The source of this contradiction lies in the fact that the true 1D superfluid ground state is an extended critical phase exhibiting algebraic decay, however, a finite bond-dimension MPS can only capture exponentially decaying correlations. As a result, the finite bond dimension effectively introduces a length scale into the system in a similar manner as finite-size effects. We can see this clearly by increasing the bond dimension. We also see that the correlation length seems to depend algebraically on the bond dimension as expected from finite-entanglement scaling arguments.
@@ -119,36 +119,42 @@ mu, t = 0.5, 0.2
 states = Vector{InfiniteMPS}(undef, length(Ds))
 
 Threads.@threads for idx in eachindex(Ds)
-    states[idx] = get_ground_state(mu, t, cutoff, Ds[idx], tol=1e-7, verbosity=1, maxiter=500)
+    states[idx] = get_ground_state(mu, t, cutoff, Ds[idx], tol = 1.0e-7, verbosity = 1, maxiter = 500)
 end
 
 npoints = 400
 two_point_correlation = zeros(length(Ds), npoints)
-a_op = a_min(cutoff=cutoff)
+a_op = a_min(cutoff = cutoff)
 
 Threads.@threads for idx in eachindex(Ds)
     two_point_correlation[idx, :] .= real.(expectation_value(states[idx], (1, i) => a_op' ⊗ a_op) for i in 1:npoints)
 end
 
-p = plot(framestyle=:box, ylabel="Correlation function " * L"\langle a_i^{\dagger}a_j \rangle",
-    xlabel="Distance " * L"|i-j|", xscale=:log10, yscale=:log10,
-    xticks=([10, 100], ["10", "100"]),
-    yticks=([0.25, 0.5, 1.0], ["0.25", "0.5", "1.0"]))
+p = plot(
+    framestyle = :box, ylabel = "Correlation function " * L"\langle a_i^{\dagger}a_j \rangle",
+    xlabel = "Distance " * L"|i-j|", xscale = :log10, yscale = :log10,
+    xticks = ([10, 100], ["10", "100"]),
+    yticks = ([0.25, 0.5, 1.0], ["0.25", "0.5", "1.0"])
+)
 
-plot!(p, 2:npoints, two_point_correlation[:, 2:end]',
-    lab="D = " .* string.(permutedims(Ds)), lw=2)
+plot!(
+    p, 2:npoints, two_point_correlation[:, 2:end]',
+    lab = "D = " .* string.(permutedims(Ds)), lw = 2
+)
 
-scatter!(p, Ds, correlation_length.(states),
-    ylabel="Correlation length", xlabel="Bond dimension",
-    xscale=:log10, yscale=:log10,
-    inset=bbox(0.2, 0.51, 0.25, 0.25),
-    subplot=2,
-    xticks=(20:10:50, string.(20:10:50)),
-    yticks=([50, 100], string.([50, 100])),
-    xlabelfontsize=8,
-    ylabelfontsize=8,
-    ylims=[20, 130],
-    xlims=[15, 60])
+scatter!(
+    p, Ds, correlation_length.(states),
+    ylabel = "Correlation length", xlabel = "Bond dimension",
+    xscale = :log10, yscale = :log10,
+    inset = bbox(0.2, 0.51, 0.25, 0.25),
+    subplot = 2,
+    xticks = (20:10:50, string.(20:10:50)),
+    yticks = ([50, 100], string.([50, 100])),
+    xlabelfontsize = 8,
+    ylabelfontsize = 8,
+    ylims = [20, 130],
+    xlims = [15, 60]
+)
 
 md"""
 This shows that any finite bond dimension MPS necessarily breaks the symmetry of the system, forming a Bose-Einstein condensate which introduces erraneous long-distance behaviour of correlation functions. In case of finite bond dimension, it is thus reasonable to associate the finite expectation value of the field operator to the 'quasicondensate' density of the system which vanishes as $D \to \infty$. 
@@ -186,11 +192,15 @@ However, we know that a finite bond dimension MPS introduces a non-zero quasi-co
 ks = range(-0.05, 0.15, 500)
 momentum_distribution = map(
     ((corr, qc),) -> sum(
-        2 .* cos.(ks' .* (2:npoints)) .* (corr[2:end] .- qc), dims=1) .+ (corr[1] .- qc),
-    zip(eachrow(two_point_correlation),
-        quasicondensate_density))
+        2 .* cos.(ks' .* (2:npoints)) .* (corr[2:end] .- qc), dims = 1
+    ) .+ (corr[1] .- qc),
+    zip(
+        eachrow(two_point_correlation),
+        quasicondensate_density
+    )
+)
 momentum_distribution = vcat(momentum_distribution...)'
-plot(ks, momentum_distribution, lab="D = " .* string.(permutedims(Ds)), lw=1.5, xlabel="Momentum k", ylabel=L"\langle n_k \rangle", ylim=[0, 50])
+plot(ks, momentum_distribution, lab = "D = " .* string.(permutedims(Ds)), lw = 1.5, xlabel = "Momentum k", ylabel = L"\langle n_k \rangle", ylim = [0, 50])
 
 md"""
 We see that the density seems to peak around $k=0$, this time seemingly becoming more prominent as $D \to \infty$ which seems to suggest again that there is a condensate. However, going by the Penrose-Onsager criterion, the existence of a condensate can be quantified by requiring the leading eigenvalue of the single particle density matrix (i.e, $\langle \hat{n}_{k=0}\rangle = \sum_j \langle \hat{a}_j^{\dagger} \hat{a}_0\rangle$) to diverge as $O(N)$ in the thermodynamic limit. In this case, since the correlations decay as a power law, there is naturally a divergence at low momenta. But this does not imply the existence of a condensate since the order of divergence is much weaker. However, this does indicate the remnants of some kind of condensation in the 1D model despite the quantum fluctuations, leading to the practical utility of defining the concept of a quasicondensate where there is still a notion of phase coherence over short distances.
@@ -202,38 +212,40 @@ $$\frac{E[\Phi] - E[0]}{L} \approx \frac{1}{2} \Upsilon(L) \bigg (\frac{\Phi}{L}
 In order to find the ground state under these twisted boundary conditions, we must construct our own variant of the Bose-Hubbard Hamiltonian. Typically you would want to take a peek at the [source code](https://github.com/QuantumKitHub/MPSKitModels.jl/blob/main/src/models/hamiltonians.jl#L379) of `MPSKitModels.jl` to see how these models are defined and tweak it as per your needs. Here we see that applying twisted boundary conditions is equivalent to adding a prefactor of $e^{\pm i\phi}$ in front of the hopping amplitudes.
 """
 
-function bose_hubbard_model_twisted_bc(elt::Type{<:Number}=ComplexF64, symmetry::Type{<:Sector}=Trivial,
-    lattice::AbstractLattice=InfiniteChain(1);
-    cutoff::Integer=5, t=1.0, U=1.0, mu=0.0, phi=0)
+function bose_hubbard_model_twisted_bc(
+        elt::Type{<:Number} = ComplexF64, symmetry::Type{<:Sector} = Trivial,
+        lattice::AbstractLattice = InfiniteChain(1);
+        cutoff::Integer = 5, t = 1.0, U = 1.0, mu = 0.0, phi = 0
+    )
 
-    a_pm = a_plusmin(elt, symmetry; cutoff=cutoff)
-    a_mp = a_minplus(elt, symmetry; cutoff=cutoff)
-    N = a_number(elt, symmetry; cutoff=cutoff)
+    a_pm = a_plusmin(elt, symmetry; cutoff = cutoff)
+    a_mp = a_minplus(elt, symmetry; cutoff = cutoff)
+    N = a_number(elt, symmetry; cutoff = cutoff)
 
     interaction_term = N * (N - id(domain(N)))
 
-    H = @mpoham begin
+    return H = @mpoham begin
         sum(nearest_neighbours(lattice)) do (i, j)
-            return -t * (exp(1im * phi) * a_pm{i,j} + exp(1im * -phi) * a_mp{i,j})
+            return -t * (exp(1im * phi) * a_pm{i, j} + exp(1im * -phi) * a_mp{i, j})
         end +
-        sum(vertices(lattice)) do i
+            sum(vertices(lattice)) do i
             return U / 2 * interaction_term{i} - mu * N{i}
         end
     end
 end
 
-function superfluid_stiffness_profile(t, mu, D, cutoff, ϵ=1e-4, npoints=11)
+function superfluid_stiffness_profile(t, mu, D, cutoff, ϵ = 1.0e-4, npoints = 11)
     phis = range(-ϵ, ϵ, npoints)
     energies = zeros(length(phis))
 
     Threads.@threads for idx in eachindex(phis)
-        hamiltonian_twisted = bose_hubbard_model_twisted_bc(cutoff=cutoff, t=t, mu=mu, U=1, phi=phis[idx])
+        hamiltonian_twisted = bose_hubbard_model_twisted_bc(cutoff = cutoff, t = t, mu = mu, U = 1, phi = phis[idx])
         state = InfiniteMPS(ℂ^(cutoff + 1), ℂ^D)
-        state_twisted, _, _ = find_groundstate(state, hamiltonian_twisted, VUMPS(; tol=1e-8, verbosity=0))
+        state_twisted, _, _ = find_groundstate(state, hamiltonian_twisted, VUMPS(; tol = 1.0e-8, verbosity = 0))
         energies[idx] = real(expectation_value(state_twisted, hamiltonian_twisted))
     end
 
-    plot(phis, energies, lw=2, xlabel="Phase twist per site" * L"(\phi)", ylabel="Ground state energy", title="t = $t | μ = $mu | D = $D | cutoff = $cutoff")
+    return plot(phis, energies, lw = 2, xlabel = "Phase twist per site" * L"(\phi)", ylabel = "Ground state energy", title = "t = $t | μ = $mu | D = $D | cutoff = $cutoff")
 end
 
 superfluid_stiffness_profile(0.2, 0.3, 5, 4) # superfluid
@@ -248,17 +260,17 @@ cutoff, D = 4, 10
 mus = range(0, 0.75, 40)
 ts = range(0, 0.3, 40)
 
-a_op = a_min(cutoff=cutoff)
+a_op = a_min(cutoff = cutoff)
 order_parameters = zeros(length(ts), length(mus))
 
 Threads.@threads for (i, j) in collect(Iterators.product(eachindex(mus), eachindex(ts)))
-    hamiltonian = bose_hubbard_model(InfiniteChain(), cutoff=cutoff, U=1, mu=mus[i], t=ts[j])
+    hamiltonian = bose_hubbard_model(InfiniteChain(), cutoff = cutoff, U = 1, mu = mus[i], t = ts[j])
     init_state = InfiniteMPS(ℂ^(cutoff + 1), ℂ^D)
-    state, _, _ = find_groundstate(init_state, hamiltonian, VUMPS(; tol=1e-8, verbosity=0))
+    state, _, _ = find_groundstate(init_state, hamiltonian, VUMPS(; tol = 1.0e-8, verbosity = 0))
     order_parameters[i, j] = abs(expectation_value(state, 0 => a_op))
 end
 
-heatmap(ts, mus, order_parameters, xlabel=L"t/U", ylabel=L"\mu/U", title=L"\langle \hat{a}_i \rangle")
+heatmap(ts, mus, order_parameters, xlabel = L"t/U", ylabel = L"\mu/U", title = L"\langle \hat{a}_i \rangle")
 
 md"""
 Although the bond dimension here is quite low, we already see the deformation of the Mott insulator lobes to give way to the well known BKT transition that happens at commensurate density. One can go further and estimate the critical exponents using finite-entanglement scaling procedures on the correlation functions, but these may now be performed with ease using what we have learnt in this tutorial.
