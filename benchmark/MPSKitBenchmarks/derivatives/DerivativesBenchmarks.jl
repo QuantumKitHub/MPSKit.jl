c9727dfa3744d303d1ae5046be47418534cea992
Lukas Devos <ldevos98@gmail.com>
2025-11-02 18:12:26 -0500

    small updates

--- benchmark/MPSKitBenchmarks/derivatives/DerivativesBenchmarks.jl
+++ benchmark/MPSKitBenchmarks/derivatives/DerivativesBenchmarks.jl
@@ -13,7 +13,7 @@ import ..BenchUtils: tomlify, untomlify
 const SUITE = BenchmarkGroup()
 
 const allparams = Dict(
-    "heisenberg_NN" => TOML.parsefile(joinpath(@__DIR__, "heisenberg_NN_specs.toml"))
+    "heisenberg_NN" => TOML.parsefile(joinpath(@__DIR__, "heisenberg_NN.toml"))
 )
 
 include("AC2_benchmarks.jl")
@@ -26,12 +26,15 @@ suite_apply = addgroup!(SUITE, "AC2_contraction")
 for (model, params) in allparams
     g_prep = addgroup!(suite_init, model)
     g_contract = addgroup!(suite_apply, model)
-    specs = untomlify.(AC2Spec, params["specs"])
-
-    for spec in specs
-        name = benchname(spec)
-        g_prep[name] = preparation_benchmark(spec; T)
-        g_contract[name] = contraction_benchmark(spec; T)
+    for (symmetry, specs) in params
+        g_prep_sym = addgroup!(g_prep, symmetry)
+        g_contract_sym = addgroup!(g_contract, symmetry)
+        for spec_dict in specs
+            spec = untomlify(AC2Spec, spec_dict)
+            name = benchname(spec)
+            g_prep_sym[name] = preparation_benchmark(spec; T)
+            g_contract_sym[name] = contraction_benchmark(spec; T)
+        end
     end
 end
 
