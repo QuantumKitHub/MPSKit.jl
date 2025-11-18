839d06a567262bb0d962805d9f947a411a649c37
AFeuerpfeil <andreas.feuerpfeil@gmail.com>
2025-11-18 11:45:47 -0500

    fix formatting

--- src/utility/styles.jl
+++ src/utility/styles.jl
@@ -10,10 +10,10 @@ Trait to describe the operator behavior of the input `x` or type `T`, which can 
 abstract type OperatorStyle end
 OperatorStyle(x) = OperatorStyle(typeof(x))
 OperatorStyle(T::Type) = throw(MethodError(OperatorStyle, T)) # avoid stackoverflow if not defined
-OperatorStyle(x::OperatorStyle) = x 
+OperatorStyle(x::OperatorStyle) = x
 
 OperatorStyle(x, y) = OperatorStyle(OperatorStyle(x)::OperatorStyle, OperatorStyle(y)::OperatorStyle)
-OperatorStyle(::T, ::T) where {T<:OperatorStyle} = T()
+OperatorStyle(::T, ::T) where {T <: OperatorStyle} = T()
 OperatorStyle(x::OperatorStyle, y::OperatorStyle) = throw(MethodError(OperatorStyle, (x, y)))
 @inline OperatorStyle(x, y, zs...) = OperatorStyle(OperatorStyle(x, y), zs...)
 
@@ -38,7 +38,7 @@ GeometryStyle(T::Type) = throw(MethodError(GeometryStyle, T)) # avoid stackoverf
 GeometryStyle(x::GeometryStyle) = x
 
 GeometryStyle(x, y) = GeometryStyle(GeometryStyle(x)::GeometryStyle, GeometryStyle(y)::GeometryStyle)
-GeometryStyle(::T, ::T) where {T<:GeometryStyle} = T()
+GeometryStyle(::T, ::T) where {T <: GeometryStyle} = T()
 GeometryStyle(x::GeometryStyle, y::GeometryStyle) = throw(MethodError(GeometryStyle, (x, y)))
 @inline GeometryStyle(x, y, zs...) = GeometryStyle(GeometryStyle(x, y), zs...)
 
