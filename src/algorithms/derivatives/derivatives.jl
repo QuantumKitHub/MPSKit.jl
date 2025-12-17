eca22fd44148c043c0fd983a46e227611888167c
Lukas Devos <ldevos98@gmail.com>
2025-12-16 23:04:51 -0500

    Add buffer allocator

--- src/algorithms/derivatives/derivatives.jl
+++ src/algorithms/derivatives/derivatives.jl
@@ -222,10 +222,10 @@ Base.:*(h::LazySum{<:Union{DerivativeOrMultiplied}}, v) = h(v)
 Given an operator and vector, try to construct a more efficient representation of that operator for repeated application.
 This should always be used in conjunction with [`unprepare_operator!!`](@ref).
 """
-prepare_operator!!(O, backend::AbstractBackend = DefaultBackend(), allocator = DefaultAllocator()) = O
+prepare_operator!!(O, backend::AbstractBackend = DefaultBackend(), allocator = GrowingBuffer()) = O
 
 # to make benchmark scripts run
-prepare_operator!!(O, x::AbstractTensorMap, backend::AbstractBackend = DefaultBackend(), allocator = DefaultAllocator()) =
+prepare_operator!!(O, x::AbstractTensorMap, backend::AbstractBackend = DefaultBackend(), allocator = GrowingBuffer()) =
     prepare_operator!!(O, backend, allocator), x
-unprepare_operator!!(y, O, x, backend::AbstractBackend = DefaultBackend(), allocator = DefaultAllocator()) =
+unprepare_operator!!(y, O, x, backend::AbstractBackend = DefaultBackend(), allocator = GrowingBuffer()) =
     y
