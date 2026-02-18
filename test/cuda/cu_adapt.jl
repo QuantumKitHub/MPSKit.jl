bcbba497bea7a343cd34abd0565e865891ef7597
Katharine Hyatt <kslimes@gmail.com>
2026-02-18 14:57:37 +0100

    Skeletal tests for CuArray-backed MPSKit

---
+++ test/cuda/cu_adapt.jl
@@ -0,0 +1,30 @@
+using Test
+using MPSKit
+using TensorKit
+using CUDA, cuTENSOR, Adapt
+
+@testset "Adapting to CuArray" begin
+    # start from random operators
+    L = 4
+    T = ComplexF64
+
+    for V in (ℂ^2, U1Space(0 => 1, 1 => 1))
+        O₁ = rand(T, V^L, V^L)
+        O₂ = rand(T, space(O₁))
+        O₃ = rand(real(T), space(O₁))
+
+        # create MPO and convert it back to see if it is the same
+        mpo₁ = adapt(CuArray, FiniteMPO(O₁))
+        mpo₂ = adapt(CuArray, FiniteMPO(O₂))
+        mpo₃ = adapt(CuArray, FiniteMPO(O₃))
+
+        @test isfinite(mpo₁)
+        @test isfinite(typeof(mpo₁))
+        @test GeometryStyle(typeof(mpo₁)) == FiniteChainStyle()
+        @test GeometryStyle(mpo₁) == FiniteChainStyle()
+        @test OperatorStyle(typeof(mpo₁)) == MPOStyle()
+        @test TensorKit.storagetype(mpo₁) == CuVector{T, 1, CUDA.DeviceMemory}
+        @test TensorKit.storagetype(mpo₂) == CuVector{T, 1, CUDA.DeviceMemory}
+        @test TensorKit.storagetype(mpo₃) == CuVector{T, 1, CUDA.DeviceMemory}
+    end
+end
