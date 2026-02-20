49bd34d499c11e00483a95c99a9068bdbaccbfe4
Boris De Vos <boris.devos@ugent.be>
2026-02-20 15:27:00 +0100

    tests

--- test/misc/makie.jl
+++ test/misc/makie.jl
@@ -1,6 +1,6 @@
 println("
 -----------------------------------
-|     Plot tests with Plots.jl    |
+|     Plot tests with Makie.jl    |
 -----------------------------------
 ")
 
@@ -9,10 +9,10 @@ using Test, TestExtras
 using MPSKit
 using TensorKit
 using TensorKit: ℙ
-using Plots
+using CairoMakie
 
 @testset "plot tests" begin
     ψ = InfiniteMPS([ℙ^2], [ℙ^5])
-    @test transferplot(ψ) isa Plots.Plot
-    @test entanglementplot(ψ) isa Plots.Plot
+    @test transferplot(ψ) isa CairoMakie.Plot
+    @test entanglementplot(ψ) isa CairoMakie.Plot
 end
