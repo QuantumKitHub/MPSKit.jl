5be4fc49208bd637267c5ca0936496d2a5cb8e1c
Boris De Vos <boris.devos@ugent.be>
2026-05-24 18:15:33 +0200

    docstrings

--- src/utility/plotting.jl
+++ src/utility/plotting.jl
@@ -9,10 +9,11 @@ Plot the [entanglement spectrum](@ref entanglement_spectrum) of a given MPS `sta
 # Keyword Arguments
 - `site::Int = 0`: MPS index for multisite unit cells. The spectrum is computed for the bond
   between `site` and `site + 1`.
-- `expand_symmetry::Logical = false`: add quantum dimension degeneracies.
+- `expand_symmetry::Bool = false`: add quantum dimension degeneracies.
 - `sortby = maximum`: the method of sorting the sectors.
 - `sector_margin = 1//10`: the amount of whitespace between sectors.
 - `sector_formatter = string`: how to convert sectors to strings.
+- `plotkwargs = (; )`: Relevant to Makie. Kwargs for the underlying plot, e.g. `plotkwargs = (; title = "custom title", xlabel = L"latexstring", xticks = (1:2, ["a", "b"]))`. For Plots, these kwargs can be passed directly to `entanglementplot` instead of via `plotkwargs`.
 
 !!! note
     You will need to manually import any plotting backend of [Makie.jl](https://github.com/MakieOrg/Makie.jl) or
@@ -21,7 +22,7 @@ Plot the [entanglement spectrum](@ref entanglement_spectrum) of a given MPS `sta
 function entanglementplot end
 
 """
-    transferplot(above, below = above; sectors = [], transferkwargs = (;))
+    transferplot(above, below = above; sectors = [], transferkwargs = (;), plotkwargs = (;))
 
 Plot the partial transfer matrix spectrum of two InfiniteMPS's.
 
@@ -31,7 +32,8 @@ Plot the partial transfer matrix spectrum of two InfiniteMPS's.
 
 # Keyword Arguments
 - `sectors = []`: vector of sectors for which to compute the spectrum. If nothing is passed, the spectrum is computed for the trivial sector.
-- `transferkwargs`: kwargs for call to [`transfer_spectrum`](@ref).
+- `transferkwargs`: kwargs for call to [`transfer_spectrum`](@ref). This needs to be passed as e.g. `transferkwargs = (; num_vals = 10)`.
+- `plotkwargs = (; )`: Relevant to Makie. Kwargs for the underlying plot, e.g. `plotkwargs = (; title = "custom title", xlabel = L"latexstring", xticks = (1:2, ["a", "b"]))`. For Plots, these kwargs can be passed directly to `transferplot` instead of via `plotkwargs`.
 - `thetaorigin = 0`: origin of the angle range.
 - `sector_formatter = string`: how to convert sectors to strings.
 
