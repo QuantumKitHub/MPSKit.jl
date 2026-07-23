404fdbb9f3bdcc73a6f9f081770dfb50815a8848
Yue Zhengyuan <yuezy1997@icloud.com>
2026-07-24 07:22:06 +0800

    Switch to left-to-right zipper

--- src/algorithms/approximate/zipper.jl
+++ src/algorithms/approximate/zipper.jl
@@ -1,7 +1,7 @@
 """
     Zipper(; alg_svd=Defaults.alg_svd(), trscheme)
 
-Algorithm that approximates an open-boundary finite MPO-MPS product using a right-to-left
+Algorithm that approximates an open-boundary finite MPO-MPS product using a left-to-right
 zipper sweep. The MPO and MPS are contracted one site at a time, and the enlarged virtual
 bond is truncated immediately using `trscheme`.
 
@@ -30,27 +30,27 @@ function approximate((O, ψ)::Tuple{Any, <:FiniteMPS}, alg::Zipper)
     T = TensorOperations.promote_contract(scalartype(O), scalartype(ψ))
     A = TensorKit.similarstoragetype(eltype(ψ), T)
 
-    Fᵣ = fuser(A, right_virtualspace(ψ, N), right_virtualspace(O, N))
+    Fₗ = fuser(A, left_virtualspace(ψ, 1), left_virtualspace(O, 1))
     local carry
     alg_gauge = MatrixAlgebraKit.TruncatedAlgorithm(alg.alg_svd, alg.trscheme)
 
-    As = map(N:-1:1) do i
+    As = map(1:N) do i
         Aψ = i == 1 ? ψ.AC[1] : ψ.AR[i]
         physicalspace(Aψ) == physicalspace(O[i]) ||
             throw(SpaceMismatch("MPO input physical space does not match MPS physical space at site $i"))
-        Fₗ = fuser(A, left_virtualspace(ψ, i), left_virtualspace(O, i))
+        Fᵣ = fuser(A, right_virtualspace(ψ, i), right_virtualspace(O, i))
         Aᶻ = _fuse_mpo_mps(O[i], Aψ, Fₗ, Fᵣ)
-        i < N && (Aᶻ = Aᶻ * carry)
+        i > 1 && (Aᶻ = _mul_front(carry, Aᶻ))
 
-        if i == 1
+        if i == N
             return Aᶻ
         else
-            C, AR = right_gauge(Aᶻ, alg_gauge)
+            AL, C, _ = left_gauge(Aᶻ, alg_gauge)
             carry = C
-            Fᵣ = Fₗ
-            return AR
+            Fₗ = Fᵣ
+            return AL
         end
     end
 
-    return FiniteMPS(reverse(As); normalize = false, overwrite = true)
+    return FiniteMPS(As; normalize = false, overwrite = true)
 end
