46ca0b743d05b5fbdb068dae7178d8169c7650bd
Yue Zhengyuan <yuezy1997@icloud.com>
2026-07-24 07:08:17 +0800

    Remove unnecessary copy

--- src/algorithms/approximate/zipper.jl
+++ src/algorithms/approximate/zipper.jl
@@ -27,19 +27,18 @@ function approximate((O, ψ)::Tuple{Any, <:FiniteMPS}, alg::Zipper)
         throw(ArgumentError("Zipper is only implemented for open-boundary MPOs"))
     end
 
-    ψ′ = copy(ψ)
     T = TensorOperations.promote_contract(scalartype(O), scalartype(ψ))
     A = TensorKit.similarstoragetype(eltype(ψ), T)
 
-    Fᵣ = fuser(A, right_virtualspace(ψ′, N), right_virtualspace(O, N))
+    Fᵣ = fuser(A, right_virtualspace(ψ, N), right_virtualspace(O, N))
     local carry
     alg_gauge = MatrixAlgebraKit.TruncatedAlgorithm(alg.alg_svd, alg.trscheme)
 
     As = map(N:-1:1) do i
-        Aψ = i == 1 ? ψ′.AC[1] : ψ′.AR[i]
+        Aψ = i == 1 ? ψ.AC[1] : ψ.AR[i]
         physicalspace(Aψ) == physicalspace(O[i]) ||
             throw(SpaceMismatch("MPO input physical space does not match MPS physical space at site $i"))
-        Fₗ = fuser(A, left_virtualspace(ψ′, i), left_virtualspace(O, i))
+        Fₗ = fuser(A, left_virtualspace(ψ, i), left_virtualspace(O, i))
         Aᶻ = _fuse_mpo_mps(O[i], Aψ, Fₗ, Fᵣ)
         i < N && (Aᶻ = Aᶻ * carry)
 
