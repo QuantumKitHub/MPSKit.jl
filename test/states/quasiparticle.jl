76ad985b634973c83c75f9527d996aba96a95fad
Boris De Vos <boris.devos@ugent.be>
2026-09-03 10:54:06 +0200

    undo states splitting

--- test/states/quasiparticle.jl
+++ test/states/quasiparticle.jl
@@ -1,13 +1,13 @@
 println("
 -----------------------------------
-|   Quasiparticle tests (finite)  |
+|   Quasiparticle tests           |
 -----------------------------------
 ")
 
 using .TestSetup
 using Test, TestExtras
 using MPSKit
-using MPSKit: GeometryStyle, FiniteChainStyle
+using MPSKit: GeometryStyle, FiniteChainStyle, InfiniteChainStyle
 using TensorKit
 using TensorKit: ℙ
 
@@ -19,6 +19,15 @@ quasiparticle_finite_cases = [
     ),
 ]
 fast_tests && (quasiparticle_finite_cases = quasiparticle_finite_cases[1:1])
+
+quasiparticle_infinite_cases = [
+    (force_planar(transverse_field_ising()), ℙ^10, ℙ^2),
+    (
+        heisenberg_XXX(ComplexF64, SU2Irrep; spin = 1), Rep[SU₂](1 => 3, 0 => 2),
+        Rep[SU₂](1 => 1),
+    ),
+]
+fast_tests && (quasiparticle_infinite_cases = quasiparticle_infinite_cases[1:1])
 
 @testset "Quasiparticle state" verbose = true begin
     L = 10
@@ -56,5 +65,37 @@ fast_tests && (quasiparticle_finite_cases = quasiparticle_finite_cases[1:1])
         ev_f = expectation_value(ϕ₁_f, H) - expectation_value(ψ, H)
         ev_q = dot(ϕ₁, MPSKit.effective_excitation_hamiltonian(H, ϕ₁))
         @test ev_f ≈ ev_q atol = 1.0e-5
+    end
+
+    @testset "Infinite" for (th, D, d) in quasiparticle_infinite_cases
+        period = rand(1:4)
+        ψ = InfiniteMPS(fill(d, period), fill(D, period))
+
+        @test eltype(ψ) == eltype(typeof(ψ))
+
+        #rand_quasiparticle is a private non-exported function
+        ϕ₁ = LeftGaugedQP(rand, ψ)
+        ϕ₂ = LeftGaugedQP(rand, ψ)
+
+        @test GeometryStyle(ϕ₁) == InfiniteChainStyle()
+        @test GeometryStyle(typeof(ϕ₂)) == InfiniteChainStyle()
+
+        @test @constinferred physicalspace(ϕ₁) == physicalspace(ψ)
+        @test @constinferred left_virtualspace(ϕ₁) == left_virtualspace(ψ)
+        @test @constinferred right_virtualspace(ϕ₁) == right_virtualspace(ψ)
+        for i in 1:period
+            @test physicalspace(ψ, i) == physicalspace(ϕ₁, i)
+            @test left_virtualspace(ψ, i) == left_virtualspace(ϕ₁, i)
+            @test right_virtualspace(ψ, i) == right_virtualspace(ϕ₁, i)
+        end
+
+        @test norm(axpy!(1, ϕ₁, copy(ϕ₂))) ≤ norm(ϕ₁) + norm(ϕ₂)
+        @test norm(ϕ₁) * 3 ≈ norm(ϕ₁ * 3)
+
+        @test dot(
+            ϕ₁,
+            convert(LeftGaugedQP, convert(RightGaugedQP, ϕ₁))
+        ) ≈
+            dot(ϕ₁, ϕ₁) atol = 1.0e-10
     end
 end
